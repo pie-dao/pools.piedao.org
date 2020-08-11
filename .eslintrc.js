8072cbc084f10114b6135f6267c55e6036e7b608
Dan Matthews <dan@cryptobuilders.io>
2020-08-11 21:11:35 +0300

    add ES6 linting configuration

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+  },
+};
