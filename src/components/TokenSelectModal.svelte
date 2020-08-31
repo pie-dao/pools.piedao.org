a227e10520ed8688bc69cc925d941e0d8a05d4b0
alexintosh <alessio.d@gmail.com>
2020-08-31 15:15:18 +0200

    Close on click background and css fix on positioning

--- src/components/TokenSelectModal.svelte
+++ src/components/TokenSelectModal.svelte
@@ -5,10 +5,30 @@
   export let tokens;
 
   export let open;
+
+  /** Dispatch event on click outside of node */
+  export function clickOutside(node) {
+    
+    const handleClick = event => {
+      if (node && !node.contains(event.target) && !event.defaultPrevented) {
+        node.dispatchEvent(
+          new CustomEvent('click_outside', node)
+        )
+      }
+    }
+
+    document.addEventListener('click', handleClick, true);
+    
+    return {
+      destroy() {
+        document.removeEventListener('click', handleClick, true);
+      }
+    }
+  }
 </script>
 
 {#if open}
-  <div class="token-select-container bg-white fixed w-300px -ml-150px rounded-4px">
+  <div class="token-select-container bg-white fixed w-300px -ml-150px rounded-4px" use:clickOutside on:click_outside={() => callback()}>
     <div class="top bg-grey-204 px-20px py-6px flex rounded-t-4px">
       <h3 class="text-md">Select a Token</h3>
       <button on:click={() => callback()} class="ml-auto">
