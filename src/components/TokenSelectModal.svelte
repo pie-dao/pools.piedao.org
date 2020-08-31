a0caec551b7eb42ffc165f093fe938394ca01e2d
Dan Matthews <dan@cryptobuilders.io>
2020-08-31 15:20:17 +0300

    style fixes

--- src/components/TokenSelectModal.svelte
+++ src/components/TokenSelectModal.svelte
@@ -1,62 +1,39 @@
 <script>
+  import images from "../config/images.json";
+
   export let callback;
   export let tokens;
 
   export let open;
 </script>
 
-<style>
-  .token-select-container {
-    z-index: 100;
-    left: 50%;
-  }
-
-  td,
-  th {
-    padding: 10px 0;
-  }
-
-  td:first-child,
-  th:first-child {
-    padding-left: 20px;
-  }
-
-  td:nth-child(2),
-  th:nth-child(2) {
-    padding-right: 20px;
-  }
-
-  tr {
-    border-bottom: 1px solid;
-  }
-</style>
-
 {#if open}
-  <div class="token-select-container bg-white fixed w-400px -ml-200px">
-    <div class="top bg-grey-204 p-20px flex">
-      <h3 class="text-lg">Select a Token</h3>
-      <button on:click={() => callback()} class="ml-auto">X</button>
+  <div class="token-select-container bg-white fixed w-300px -ml-150px rounded-4px">
+    <div class="top bg-grey-204 px-20px py-6px flex rounded-t-4px">
+      <h3 class="text-md">Select a Token</h3>
+      <button on:click={() => callback()} class="ml-auto">
+        <img src={images.icons.timesLight} alt="close" class="w-8px" />
+      </button>
     </div>
-    <div class="bottom">
+    <div class="bottom rounded-b-4px">
       <table class="w-100pc">
         <thead>
-          <tr class="border-grey-243">
-            <th class="font-thin w-50pc text-left">Token Name</th>
-            <th class="font-thin w-50pc text-right">Balance</th>
+          <tr class="border-grey-243 text-xs">
+            <th class="font-thin w-50pc text-left py-10px">Token Name</th>
+            <th class="font-thin w-50pc text-right py-10px">Balance</th>
           </tr>
-          x
         </thead>
         <tbody>
           {#each tokens as token}
-            <tr class="border-grey-243" on:click={() => callback(token)}>
-              <td class="flex text-left">
+            <tr class="border-grey-243 pointer" on:click={() => callback(token)}>
+              <td class="flex text-left py-4px">
                 <img
                   src={token.icon}
                   alt={token.symbol}
-                  class="token-icon w-32px h-32px my-4px mx-2px" />
-                <span>{token.symbol}</span>
+                  class="token-icon w-24px h-24px my-4px mx-2px" />
+                <span class="leading-32px pl-2 text-md">{token.symbol}</span>
               </td>
-              <td class="text-right">{token.balance}</td>
+              <td class="text-right py-4px text-md font-thin">{token.balance}</td>
             </tr>
           {/each}
         </tbody>
