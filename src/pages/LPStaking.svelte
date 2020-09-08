f8847d98c6a2541ad83c4d8c4ba06de02e2303d2
Nicola Di Marco <ndimarco@amp.energy>
2020-09-08 08:05:41 +0200

    cards mobile

--- src/pages/LPStaking.svelte
+++ src/pages/LPStaking.svelte
@@ -7,9 +7,9 @@
     <h1 class="mt-8 mb-4 text-base md:text-xl">Select a Pool</h1>
     <div class="flex flex-col w-full justify-center md:flex-row">
 
-        <div class="farming-card flex flex-col justify-center align-center items-center text-center md:m-2 border border-gray border-opacity-50 border-solid rounded-sm p-6">
+        <div class="farming-card flex flex-col justify-center align-center items-center text-center mx-2 my-2 md:m-2 border border-gray border-opacity-50 border-solid rounded-sm p-6">
             <img class="h-40px w-40px mb-2 md:h-70px md:w-70px"src={images.logos.piedao_clean} alt="PieDAO logo" />
-            <div class="title text-lg"> ETH pool Balancer</div>
+            <div class="title text-lg"> ETH pool Balancer 1</div>
             <div class="subtitle font-thin">sometihng goes here</div>
             <div class="apy">APY 15%</div>
             <a href="#button">
@@ -17,9 +17,9 @@
             </a>
         </div>
 
-        <div class="farming-card flex flex-col justify-center align-center items-center text-center md:m-2 border border-gray border-opacity-50 border-solid rounded-sm p-6">
+        <div class="farming-card flex flex-col justify-center align-center items-center text-center m2-4 my-2 md:m-2 border border-gray border-opacity-50 border-solid rounded-sm p-6">
             <img class="h-40px w-40px mb-2 md:h-70px md:w-70px"src={images.logos.piedao_clean} alt="PieDAO logo" />
-            <div class="title text-lg"> ETH pool Balancer</div>
+            <div class="title text-lg"> ETH pool Balancer 2</div>
             <div class="subtitle font-thin">sometihng goes here</div>
             <div class="apy">APY 15%</div>
             <a href="#button">
@@ -27,9 +27,9 @@
             </a>
         </div>
 
-        <div class="farming-card flex flex-col justify-center align-center items-center text-center md:m-2 border border-gray border-opacity-50 border-solid rounded-sm p-6">
+        <div class="farming-card flex flex-col justify-center align-center items-center text-center mx-2 my-2 md:m-2 border border-gray border-opacity-50 border-solid rounded-sm p-6">
             <img class="h-40px w-40px mb-2 md:h-70px md:w-70px"src={images.logos.piedao_clean} alt="PieDAO logo" />
-            <div class="title text-lg"> ETH pool Balancer</div>
+            <div class="title text-lg"> ETH pool Balancer 3</div>
             <div class="subtitle font-thin">sometihng goes here</div>
             <div class="apy">APY 15%</div>
             <a href="#button">
