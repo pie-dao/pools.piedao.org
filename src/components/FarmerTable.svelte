6292db7e4a0b3c5d70e70c36c9f12e35647f3bd1
alexintosh <alessio.d@gmail.com>
2020-10-07 23:11:23 +0200

    Uniswap apr

--- src/components/FarmerTable.svelte
+++ src/components/FarmerTable.svelte
@@ -2,7 +2,8 @@
   import {
     getTokenImage,
     formatFiat,
-    calculateAPRBalancer
+    calculateAPRBalancer,
+    calculateAPRUniswap
   } from "../components/helpers.js";
 
   import { farming } from '../stores/eth/writables.js';
@@ -64,7 +65,7 @@
     {
       symbol: "DEFI+S/DAI",
       address: "",
-      aprEnabled: false,
+      aprEnabled: true,
       addressTokenToStake: '0x7aeFaF3ea1b465dd01561B0548c9FD969e3F76BA',
       addressUniPoll: '0x64964cb69f40A1B56AF76e32Eb5BF2e2E52a747c',
       weights: "50 / 50",
@@ -75,13 +76,13 @@
       containing: [
         {
           symbol: "DEFI+S",
-          address: "0xaD6A626aE2B43DCb1B39430Ce496d2FA0365BA9C",
+          address: "0xad6a626ae2b43dcb1b39430ce496d2fa0365ba9c",
           balance: '0',
           icon: getTokenImage('0xad6a626ae2b43dcb1b39430ce496d2fa0365ba9c')
         },
         {
           symbol: "DAI",
-          address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+          address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
           balance: '0',
           icon: getTokenImage('0x6B175474E89094C44Da98b954EedeAC495271d0F')
         },
@@ -95,7 +96,11 @@
         if(pool.aprEnabled) {
           console.log('symbol', pool.symbol);
           try {
-            await calculateAPRBalancer(pool.addressUniPoll, pool.addressTokenToStake, null, null, pool.containing[0].address, pool.containing[1].address);
+            if( pool.type === 'UniswapV2') {
+                await calculateAPRUniswap(pool.addressUniPoll, pool.addressTokenToStake, null, null, pool.containing[0].address, pool.containing[1].address);
+            } else {
+              await calculateAPRBalancer(pool.addressUniPoll, pool.addressTokenToStake, null, null, pool.containing[0].address, pool.containing[1].address);
+            }
           } catch(e) {
             console.log('e', e);
           }
