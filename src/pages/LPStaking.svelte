39af0fb8683fb5ce83842b6a7a7822562315e24e
alexintosh <alessio.d@gmail.com>
2020-11-01 23:03:35 +0100

    Avoid calling toFixed on undefined

--- src/pages/LPStaking.svelte
+++ src/pages/LPStaking.svelte
@@ -933,9 +933,11 @@
               
               {#if pool.contractType === "Geyser"}
                 <br/>
-                <p> Your stake of <strong>{geyserApy.yourStake.toFixed(2)} BPT</strong> is earning right now <strong>{geyserApy.earnedOptimistic} DOUGH</strong> assuming you will not unstake until the end of the program. </p>
-                <p>Approx <strong>{geyserApy.apy}%</strong>.</p>
-                <p>If you would unstake right now, you would get exactly <strong>{geyserApy.earnedNotOptimistic} DOUGH</strong>, which is approx <strong>{geyserApy.apyNotOptimistic}% APR</strong>.</p>
+                {#if geyserApy.yourStake}
+                  <p> Your stake of <strong>{geyserApy.yourStake.toFixed(2)} BPT</strong> is earning right now <strong>{geyserApy.earnedOptimistic} DOUGH</strong> assuming you will not unstake until the end of the program. </p>
+                  <p>Approx <strong>{geyserApy.apy}%</strong>.</p>
+                  <p>If you would unstake right now, you would get exactly <strong>{geyserApy.earnedNotOptimistic} DOUGH</strong>, which is approx <strong>{geyserApy.apyNotOptimistic}% APR</strong>.</p>
+                {/if}
               {/if}
 
               <br/><br/>
