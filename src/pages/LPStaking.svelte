7807861b31dcc46c4c15b84d9f971bf615a571fc
alexintosh <alessio.d@gmail.com>
2020-11-11 09:00:22 +0100

    fix typo

--- src/pages/LPStaking.svelte
+++ src/pages/LPStaking.svelte
@@ -496,7 +496,7 @@
                         </div>
                         <div>
                           <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                            Escrowed Dough Amount
+                            DOUGH in Escrow
                           </p>
                           <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                             {formatFiat(rewardEscrewData.totalEscrewed, ',', '.', '')}
