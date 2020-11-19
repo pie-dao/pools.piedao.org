4f3b994c10e4fe5a532dd7324fb9d45242ba3542
alexintosh <alessio.d@gmail.com>
2020-11-19 17:06:21 +0100

    HeroCentered component

--- src/components/marketing-elements/audits.svelte
+++ src/components/marketing-elements/audits.svelte
@@ -2,21 +2,23 @@
   import images from "../../config/images.json";
 </script>
 
-<div class="text-white bg-black rounded-4px py-6px px-8px mb-10px">
-    RISK-OPPORTUNITIES
+<div class="content flex flex-col w-100pc justify-between spl text-center md:w-80pc">
+    <div class="text-white bg-black rounded-4px py-6px px-8px mb-10px">
+        RISK-OPPORTUNITIES
+    </div>
+    <div class="font-veryhuge">
+        Collective Financial iQ,<br/>
+        Contracts Audits.
+    </div>
+    <div class="font-thin text-lg mt-10px mb-10px md:w-80pc">
+            We build PieDAO to capture the best insights from our community.<br/>
+        This way we can reduce biases and deploy new ideas faster.<br/>
+        All our smart contract have been officially audited.
+    </div>
+    <div class="quantstamp-container my-10px">
+        <a href="learn" target="_blank">
+            <img class="w-200px h-auto" src={images.logos.quantstamp} alt="quantumstamp logo" />
+        </a>
+    </div>
+    <a href="audits" class="font-bold text-sm mb-50px">Learn about audits ></a>
 </div>
-<div class="font-veryhuge">
-    Collective Financial iQ,<br/>
-    Contracts Audits.
-</div>
-<div class="font-thin text-lg mt-10px mb-10px md:w-80pc">
-        We build PieDAO to capture the best insights from our community.<br/>
-      This way we can reduce biases and deploy new ideas faster.<br/>
-      All our smart contract have been officially audited.
-</div>
-<div class="quantstamp-container my-10px">
-    <a href="learn" target="_blank">
-        <img class="w-200px h-auto" src={images.logos.quantstamp} alt="quantumstamp logo" />
-    </a>
-</div>
-<a href="audits" class="font-bold text-sm mb-50px">Learn about audits ></a>
--- src/components/marketing-elements/defi-pie.svelte
+++ src/components/marketing-elements/defi-pie.svelte
@@ -7,8 +7,8 @@
     Full DeFi Exposure
   </div>
     <div class="font-thin text-lg mt-10px mb-10px md:w-80pc">
-    Index investing is one of the best ways to gain passive exposure to a diversified portfolio.
-    With <strong>DXP</strong> you just set it and forget it and let the market do the work!
+      Index investing is one of the best ways to gain passive exposure to a diversified portfolio.
+      With <strong>DXP</strong> you just set it and forget it and let the market do the work!
     </div>
       <div class="my-20px">
         <img class="w-400px h-auto" src={images.defiplusicons} alt="DeFi plus icons" />
