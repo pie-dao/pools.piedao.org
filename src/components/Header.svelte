c3f155c8630382223a0959012b08abc3c8f3c2fc
alexintosh <alessio.d@gmail.com>
2020-11-20 10:28:53 +0100

    Close dropdowns when clickOutside

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,6 +4,8 @@
   import images from "../config/images.json";
   import links from "../config/links.json";
 
+  import {clickOutside} from '../helpers/clickOutside.js';
+
   import Web3Button from "./Web3Button.svelte";
   import TVL from "./Tvl.svelte";
 
@@ -27,6 +29,11 @@
     dropdownOpen2 = !dropdownOpen2;
   };
 
+  const closeDropdowns = () => {
+    dropdownOpen = false;
+    dropdownOpen2 = false;
+  };
+
 </script>
 <div class="header-container">
   <div class="left">
@@ -43,9 +50,9 @@
 
     <div class="relative inline-block text-left">
 
-      <div>
+      <div use:clickOutside on:click_outside={closeDropdowns}>
         <span class="shadow-sm">
-          <button on:click={toggleDropdow} type="button" class="inline-flex justify-center w-full px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" aria-expanded="true">
+          <button on:click={toggleDropdow}  type="button" class="inline-flex justify-center w-full px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" aria-expanded="true">
             Products
             <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
---
+++ src/helpers/clickOutside.js
@@ -0,0 +1,19 @@
+/** Dispatch event on click outside of node */
+// eslint-disable-next-line import/prefer-default-export
+export function clickOutside(node) {
+  const handleClick = (event) => {
+    if (node && !node.contains(event.target) && !event.defaultPrevented) {
+      node.dispatchEvent(
+        new CustomEvent('click_outside', node),
+      );
+    }
+  };
+
+  document.addEventListener('click', handleClick, true);
+
+  return {
+    destroy() {
+      document.removeEventListener('click', handleClick, true);
+    },
+  };
+}
