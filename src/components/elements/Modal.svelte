9aecb3e07bc381752e99939536842f4d34d8a10f
Nicola Di Marco <ndimarco@amp.energy>
2020-11-20 18:45:59 +0100

    modal paddings

--- src/components/elements/Modal.svelte
+++ src/components/elements/Modal.svelte
@@ -19,7 +19,7 @@ console.log('backgroundColor', backgroundColor)
 {#if modalIsOpen}
     <div class="genericmodal flex  justify-center items-center">
         <div style={ backgroundColor ? `background-color: ${backgroundColor} !important` : "#fff"} 
-             class="flex flex-col justify-center modalcontent w-100pc min-h-100pc overflow-x-hidden overflow-y-auto lg:max-w-50pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:clickOutside on:click_outside={() => modalIsOpen = false}>
+             class="flex flex-col justify-start py-4 modalcontent w-100pc min-h-100pc overflow-x-hidden overflow-y-auto lg:max-w-50pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:clickOutside on:click_outside={() => modalIsOpen = false}>
             <div class="flex">
              {#if title}
                 <h1 class="text-center text-xl w-100pc"> {title} </h1>
