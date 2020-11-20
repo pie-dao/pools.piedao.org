f5116cdcc687d724432ab7e990ff3896efa39ac2
Nicola Di Marco <ndimarco@amp.energy>
2020-11-20 13:34:37 +0100

    keyfacts fixes

--- src/components/KeyFacts.svelte
+++ src/components/KeyFacts.svelte
@@ -4,55 +4,55 @@
 
 </script>
 
-<div class="container mt-4">
-  <h1 class="text-xl leading-none font-black text-center">Key Facts</h1>
+<div class="container ">
+  <h1 class="text-xl leading-none font-black text-center mb-4">Key Facts</h1>
 
-  <div class="flex flex-col justify-between mt-4  lg:flex-row">
-    <div class="left flex-col justify-between keyborder mt-2 mb-2 lg:w-1/2 lg:mr-20px lg:flex-row">
-      <div class="top flex-col justify-between lg:flex-row">
+  <div class="flex flex-col justify-between   lg:flex-row">
+    <div class="left flex-col justify-between keyborder pb-2 mt-2 mb-2 lg:w-1/2 lg:mr-20px md:flex-row">
+      <div class="flex justify-between">
         <div class="titolo">Net Assets</div>
         <div class="info font-thin mb-1">$231,787,000,889</div>
       </div>
-      <div class="bottom font-thin mb-2">as of Nov 19, 2020</div>
+      <div class="bottom text-xs font-thin">as of Nov 19, 2020</div>
     </div>
 
-    <div class="right flex-col justify-between keyborder mt-2 mb-2 lg:w-1/2 lg:ml-20px lg:flex-row">
-      <div class="top flex justify-between lg:flex-col">
+    <div class="right flex-col justify-between keyborder pb-2  mt-2 mb-2 lg:w-1/2 lg:ml-20px lg:flex-row">
+      <div class="flex justify-between">
         <div class="titolo">Premium/Discount</div>
         <div class="info font-thin mb-1">0.02%</div>
       </div>
-      <div class="bottom font-thin mb-2">as of Nov 19, 2020</div>
+      <div class="bottom text-xs font-thin">as of Nov 19, 2020</div>
     </div>
   </div>
 
-  <div class="flex flex-col justify-between mt-4  lg:flex-row">
-    <div class="left flex-col justify-between keyborder mt-2 mb-2 lg:w-1/2 lg:mr-20px lg:flex-row">
-      <div class="top flex justify-between lg:flex-col">
+  <div class="flex flex-col justify-between   lg:flex-row">
+    <div class="left flex-col justify-between keyborder pb-2  mt-2 mb-2 lg:w-1/2 lg:mr-20px lg:flex-row">
+      <div class="flex justify-between">
         <div class="titolo">Shares Outstanding</div>
         <div class="info font-thin mb-1">645,450,000</div>
       </div>
-      <div class="bottom font-thin mb-2">as of Nov 19, 2020</div>
+      <div class="bottom text-xs font-thin">as of Nov 19, 2020</div>
     </div>
 
-    <div class="right flex-col justify-between keyborder mt-2 mb-2 lg:w-1/2 lg:ml-20px lg:flex-row">
-      <div class="top flex justify-between lg:flex-col">
+    <div class="right flex-col justify-between keyborder pb-2  mt-2 mb-2 lg:w-1/2 lg:ml-20px lg:flex-row">
+      <div class="flex justify-between">
         <div class="titolo">30 Day Avg. Volume</div>
         <div class="info font-thin mb-1">4,482,927.00</div>
       </div>
-      <div class="bottom font-thin mb-2">as of Nov 19, 2020</div>
+      <div class="bottom text-xs font-thin">as of Nov 19, 2020</div>
     </div>
   </div>
 
-  <div class="flex flex-col justify-between mt-4  lg:flex-row">
-    <div class="left flex-col justify-between keyborder mt-2 mb-2 lg:w-1/2 lg:mr-20px lg:flex-row">
-      <div class="top flex justify-between lg:flex-col">
+  <div class="flex flex-col justify-between   lg:flex-row">
+    <div class="left flex-col justify-between keyborder pb-2  mt-2 mb-2 lg:w-1/2 lg:mr-20px lg:flex-row">
+      <div class="flex justify-between">
         <div class="titolo">Closing Price</div>
         <div class="info font-thin mb-1">359.04</div>
       </div>
     </div>
 
-    <div class="right flex-col justify-between keyborder mt-2 mb-2 lg:w-1/2 lg:ml-20px lg:flex-row">
-      <div class="top flex justify-between lg:flex-col">
+    <div class="right flex-col justify-between keyborder pb-2  mt-2 mb-2 lg:w-1/2 lg:ml-20px lg:flex-row">
+      <div class="flex justify-between">
         <div class="titolo">Daily Volume</div>
         <div class="info font-thin mb-1">3,321,607.00</div>
       </div>
