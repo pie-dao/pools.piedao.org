ff0650f43030d815a361f18a6fb327d12386256b
Nicola Di Marco <ndimarco@amp.energy>
2020-11-20 18:44:36 +0100

    close button alignement

--- src/components/elements/Modal.svelte
+++ src/components/elements/Modal.svelte
@@ -19,13 +19,15 @@ console.log('backgroundColor', backgroundColor)
 {#if modalIsOpen}
     <div class="genericmodal flex  justify-center items-center">
         <div style={ backgroundColor ? `background-color: ${backgroundColor} !important` : "#fff"} 
-             class="flex justify-center modalcontent w-100pc min-h-100pc overflow-x-hidden overflow-y-auto lg:max-w-50pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:clickOutside on:click_outside={() => modalIsOpen = false}>
-            {#if title}
+             class="flex flex-col justify-center modalcontent w-100pc min-h-100pc overflow-x-hidden overflow-y-auto lg:max-w-50pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:clickOutside on:click_outside={() => modalIsOpen = false}>
+            <div class="flex">
+             {#if title}
                 <h1 class="text-center text-xl w-100pc"> {title} </h1>
             {/if}
-            <button on:click={close}>
+            <button on:click={close} class="w-30px h-30px mr-20px self-center">
                 X
             </button>
+        </div>
             <slot name="content">
                 
             </slot>
