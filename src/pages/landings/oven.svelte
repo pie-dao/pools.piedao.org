5ee4b2b9e52f36802c6bbf380507dcbf111228a9
alexintosh <alessio.d@gmail.com>
2020-11-27 12:31:47 +0100

    Empty landing page

---
+++ src/pages/landings/oven.svelte
@@ -0,0 +1,8 @@
+<script>
+  import images from "../../config/images.json";
+  import SectionImageRight from "../../components/elements/SectionImageRight.svelte"
+  import SectionImageLeft from "../../components/elements/SectionImageLeft.svelte"
+</script>
+
+<section class="py-12 px-4 text-center">
+</section>
