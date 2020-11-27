4606fd8251de55238670dc6d42f751ed71ba4822
alexintosh <alessio.d@gmail.com>
2020-11-27 18:27:54 +0100

    New content

---
+++ src/components/marketing-elements/usdp-explanation.svelte
@@ -0,0 +1,18 @@
+<div>
+    <h1 class="font-lg">Summary:</h1>
+    <span class="font-thin">
+        A range of centralized and decentralized services produce tokenized US dollars. Each comes with its own advantages, compromises, and risks. With USD++ you can minimise exposure to these risks with a balanced, diversified portfolio.
+    </span>
+
+    <h1 class="mt-4">Objective:</h1>
+    <div class="font-thin">
+        Tokenized USD allows users to gain exposure to a more stable and liquid asset without having to deal directly with a centralized organisation or exchange. USD++ makes this process more secure, splitting the potential damage to holders by diversifying funds evenly between four different tokens.
+    </div>
+
+    <h1 class="mt-4">Methodology:</h1>
+
+    <div class="font-thin">
+        USD++ contains four different USD-pegged tokens, whose percentage weight results as a function of several factors including their volatility compared to the peg of $1, their assessment of the trust minimization offered & the market risk involved, allowing weights to be adjusted on a periodic basis by the DAO.
+    </div>
+    
+</div>
