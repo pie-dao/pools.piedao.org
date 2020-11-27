6dcff391cc55ba8077887d4fbf02fc8984d9c184
Alessio Delmonti <alessio.d@gmail.com>
2020-11-27 18:37:19 +0100

    Beta (#50)
    * Adding content Pies
    
    * About Content
    
    * Adding Meta component
    
    * New content
    
    * New content
    
    * Comment

---
+++ src/components/elements/meta.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let metadata = {};
+</script>
+
+<svelte:head>
+  {#if metadata.title}
+    <title>{metadata.title}</title>
+    <meta name="title" content={metadata.title} />
+    <meta property="og:title" content={metadata.title} />
+    <meta property="twitter:title" content={metadata.title} />
+  {/if}
+  
+  {#if metadata.description}
+    <meta name="description" content="Page Description" />
+    <meta property="og:description" content="Page Description" />
+    <meta property="twitter:description" content="Page Description" />
+  {/if} 
+  
+  {#if metadata.image}
+    <meta property="og:image" content="example.com/path_to/thumbnail.jpg" />
+    <meta
+      property="twitter:image"
+      content="https://example.com/path-to/thumbnail.jpg"
+    />
+  {/if} 
+  
+  {#if metadata.imageAlt}
+    <meta property="og:image:alt" content={metadata.imageAlt} />
+    <meta property="twitter:image:alt" content={metadata.imageAlt} />
+  {/if}
+
+  {#if metadata.url}
+    <meta property="og:url" content={metadata.url} />
+    <meta property="twitter:url" content={metadata.url} />
+  {/if}
+
+ 	{#if metadata.type}
+	  <meta property="og:type" content={metadata.type} />
+  {/if}
+
+  <meta property="twitter:card" content={metadata.twitterCard || "summary_large_image"} />
+</svelte:head>
---
+++ src/components/marketing-elements/pie-explanation-switch.svelte
@@ -0,0 +1,33 @@
+<script>
+import DLP from './dlp-explanation.svelte'
+import DXP from './dxp-explanation.svelte'
+import DSP from './dsp-explanation.svelte'
+import BTC from './btcp-explanation.svelte'
+import USD from './usdp-explanation.svelte'
+import Meta from '../elements/meta.svelte';
+
+export let address;
+
+	
+// const metadata = {
+//     title: 'Test',
+//     description: 'It really is!',
+//     image: 'https://svelte.dev/images/twitter-card.png',
+//     imageAlt: 'Svelte svelte.dev',
+//     url:'svelte.dev'
+// }
+</script>
+
+<div>
+    {#if address === '0x78f225869c08d478c34e5f645d07a87d3fe8eb78'}
+        <DLP />
+    {:else if address === '0x8d1ce361eb68e9e05573443c407d4a3bed23b033'}
+        <DXP />
+    {:else if address === '0xad6a626ae2b43dcb1b39430ce496d2fa0365ba9c'}
+        <DSP />
+    {:else if address === '0x0327112423f3a68efdf1fcf402f6c5cb9f7c33fd'}
+        <BTC />
+    {:else if address === '0x9a48bd0ec040ea4f1d3147c025cd4076a2e71e3e'}
+        <USD />
+    {/if}
+</div>
