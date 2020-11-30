e957d8b636e9a2e118eac89cca117af1346e4397
Alessio Delmonti <alessio.d@gmail.com>
2020-11-30 17:48:59 +0100

    Feature/recipe v3 (#53)
    * Adding Gauge + Status tab
    
    * Adding link to the menu and update recipe address
    
    * BCP enabled recipe

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -325,17 +325,26 @@ export const fetchCalcTokensForAmounts = async (pieAddress, poolAmount) => {
 
 export const fetchCalcToPie = async (pieAddress, poolAmount) => {
   validateIsAddress(pieAddress);
-  const recipe = await contract({
-    address: '0xca9af520706a57cecde6f596852eabb5a0e6bb0e',
-    abi: recipeAbi,
-  });
+
+  const { provider } = get(eth);
+
+  const recipe = new ethers.Contract('0x6cb4b8669e23295563d3b34df4a760c0cee993c7', recipeAbi, provider);
+
+  // const recipe = await contract({
+  //   address: '0xca9af520706a57cecde6f596852eabb5a0e6bb0e',
+  //   abi: recipeAbi,
+  // });
 
   const amount = ethers.BigNumber.from(
     BigNumber(poolAmount)
       .multipliedBy(10 ** 18)
       .toFixed(0),
   );
-  const amountEthNecessary = await recipe.calcToPie(pieAddress, amount);
+  const amountEthNecessary = await recipe.callStatic.calcToPie(
+    pieAddress, 
+    amount
+  );
+
   return {
     val: amountEthNecessary,
     label: ethers.utils.formatEther(amountEthNecessary),
