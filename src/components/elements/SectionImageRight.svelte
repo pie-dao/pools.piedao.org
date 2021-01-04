197720a28f6ad904867d742cc66b49de933e4e71
Nicola Di Marco <ndimarco@amp.energy>
2021-01-04 12:56:35 +0100

    dough card image fixes

--- src/components/elements/SectionImageRight.svelte
+++ src/components/elements/SectionImageRight.svelte
@@ -4,9 +4,9 @@
 export let isGrey;
 </script>
 
-<div class:bg-grey-243={isGrey === true} class=" rounded-sm p-2 flex justify-between flex-wrap w-full md:p-8 lg:p-8">
+<div class:bg-grey-243={isGrey === true} class="rounded-sm p-2 flex flex-col-reverse justify-between items-center flex-wrap w-full md:p-8 md:flex-row lg:p-8 lg:flex-row">
     <div class="p-4 flex justify-center items-center content-center flex-wrap md:w-2/3">
-        <h1 class="text-lg md:text-xl">
+        <h1 class="text-lg text-center md:text-xl md:text-left lg:text-left">
             {title}
         </h1>
         <div class="font-thin text-base mt-2 md:mt-4 md:text-lg lg:text-lg">
@@ -15,7 +15,7 @@ export let isGrey;
             </slot>
         </div>
     </div>
-    <div class="p-4 flex justify-center w-full flex-wrap md:w-1/3 p-16">
-        <img class="w-100pc h-full md:h-auto md:w-100pc" src={image} alt={title} />
+    <div class="p-4 flex justify-center items-center w-full flex-wrap md:w-1/3 lg:w-1/3">
+        <img class="w-25pc md:w-200px md:h-200px lg:w-200px lg:h-200px" src={image} alt={title} />
     </div>
 </div>
