a4e6cfdb3c120b3df7ffdee3bc98cfe5c1d6e2fc
Nicola Di Marco <ndimarco@amp.energy>
2021-01-19 18:40:40 +0100

    FAQ and Oven illustration

--- src/pages/landings/oven.svelte
+++ src/pages/landings/oven.svelte
@@ -1,22 +1,19 @@
 <script>
   import { onMount } from 'svelte';
   import { ethers } from 'ethers';
-  import images from "../../config/images.json";
+  import images from '../../config/images.json';
   import ProductBox from '../../components/elements/product-box.svelte';
 
   import Meta from '../../components/elements/meta.svelte';
   import { balances, balanceKey } from '../../stores/eth.js';
-  
-  import {
-    getTokenImage,
-    subscribeToBalance,
-    toFixed
-  } from "../../components/helpers.js";
+
+  import { getTokenImage, subscribeToBalance, toFixed } from '../../components/helpers.js';
 
   import Modal from '../../components/elements/Modal.svelte';
   import LiquidityModal from '../../components/modals/LiquidityModal.svelte';
-  import OvenModal from "../../components/modals/OvenModal.svelte";
-import TooltipButton from '../../components/elements/TooltipButton.svelte';
+  import OvenModal from '../../components/modals/OvenModal.svelte';
+  import TooltipButton from '../../components/elements/TooltipButton.svelte';
+  import { Accordion, AccordionItem } from 'svelte-accessible-accordion';
 
   $: ovens = [
     {
@@ -26,13 +23,13 @@ import TooltipButton from '../../components/elements/TooltipButton.svelte';
       description: 'Bakes DEFI++ at Zero cost',
       data: {
         ethBalance: 0,
-        pieBalance: 0
+        pieBalance: 0,
       },
       baking: {
-          symbol: "DEFI++",
-          address: "0x8d1ce361eb68e9e05573443c407d4a3bed23b033",
-          balance: '0',
-          icon: getTokenImage('0x8d1ce361eb68e9e05573443c407d4a3bed23b033')
+        symbol: 'DEFI++',
+        address: '0x8d1ce361eb68e9e05573443c407d4a3bed23b033',
+        balance: '0',
+        icon: getTokenImage('0x8d1ce361eb68e9e05573443c407d4a3bed23b033'),
       },
       highlight: true,
       enabled: true,
@@ -44,13 +41,13 @@ import TooltipButton from '../../components/elements/TooltipButton.svelte';
       description: 'Bakes BCP at Zero cost',
       data: {
         ethBalance: 0,
-        pieBalance: 0
+        pieBalance: 0,
       },
       baking: {
-          symbol: "BCP",
-          address: "0xe4f726adc8e89c6a6017f01eada77865db22da14",
-          balance: '0',
-          icon: getTokenImage('0xe4f726adc8e89c6a6017f01eada77865db22da14')
+        symbol: 'BCP',
+        address: '0xe4f726adc8e89c6a6017f01eada77865db22da14',
+        balance: '0',
+        icon: getTokenImage('0xe4f726adc8e89c6a6017f01eada77865db22da14'),
       },
       highlight: true,
       enabled: true,
@@ -62,13 +59,13 @@ import TooltipButton from '../../components/elements/TooltipButton.svelte';
       description: 'Bakes YPIE at Zero cost',
       data: {
         ethBalance: 0,
-        pieBalance: 0
+        pieBalance: 0,
       },
       baking: {
-          symbol: "YPIE",
-          address: "0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31",
-          balance: '0',
-          icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31')
+        symbol: 'YPIE',
+        address: '0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31',
+        balance: '0',
+        icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31'),
       },
       highlight: true,
       enabled: true,
@@ -81,89 +78,82 @@ import TooltipButton from '../../components/elements/TooltipButton.svelte';
       description: 'Bakes YPIE at Zero cost',
       data: {
         ethBalance: 0,
-        pieBalance: 0
+        pieBalance: 0,
       },
       baking: {
-          symbol: "YPIE",
-          address: "0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31",
-          balance: '0',
-          icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31')
+        symbol: 'YPIE',
+        address: '0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31',
+        balance: '0',
+        icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31'),
       },
       highlight: true,
       enabled: true,
-    }
-  ]
+    },
+  ];
 
   let modal;
   let modalAdd;
   let modalOption = {
-    title: "Bake",
+    title: 'Bake',
     pieAddress: null,
-    ovenAddress: null
+    ovenAddress: null,
   };
 
   onMount(() => {
-    ovens.forEach(ov => {
+    ovens.forEach((ov) => {
       subscribeToBalance(null, ov.addressOven);
       ov.KeyEthBalance = balanceKey(ethers.constants.AddressZero, ov.addressOven);
     });
   });
-
-
-
 </script>
 
-  <Meta 
-    metadata={{
-      title: "Oven Page - PieDAO",
-      description: "The PieDAO Oven pools ETH to issue pies in batches, giving users access to our index products gas-free. This design makes getting involved affordable and convenient, opening participation to everyone. Don't forget, the Oven won't activate until it's full!",
-      image: images.oven_social,
-      imageAlt: 'How the Oven makes minting pies gas-free.'
-    }}
-  />
+<Meta
+  metadata={{ title: 'Oven Page - PieDAO', description: "The PieDAO Oven pools ETH to issue pies in batches, giving users access to our index products gas-free. This design makes getting involved affordable and convenient, opening participation to everyone. Don't forget, the Oven won't activate until it's full!", image: images.oven_social, imageAlt: 'How the Oven makes minting pies gas-free.' }} />
 
-  <Modal title={modalOption.title} backgroundColor="#f3f3f3" bind:this="{modal}">
-    <span slot="content">
-      <OvenModal deprecated={modal.deprecated} pieAddress={modal.pieAddress} ovenAddress={modal.ovenAddress} />
-    </span>
-  </Modal>
+<Modal title={modalOption.title} backgroundColor="#f3f3f3" bind:this={modal}>
+  <span slot="content">
+    <OvenModal
+      deprecated={modal.deprecated}
+      pieAddress={modal.pieAddress}
+      ovenAddress={modal.ovenAddress} />
+  </span>
+</Modal>
 
-  <Modal title="Add Liquidity" backgroundColor="#f3f3f3" bind:this="{modalAdd}">
-    <span slot="content">
-      <LiquidityModal 
-        token="0x8d1ce361eb68e9e05573443c407d4a3bed23b033" 
-        method="single"
-        poolAction="add"
-      />
-    </span>
-  </Modal>
-  <section class="pt-4 px-4 text-center md:pt-8 lg:pt-12">
-    <div class="w-full max-w-2xl mx-auto">
-      <h2 class="text-xl mt-2 mb-6 leading-tight font-heading">Bake Together, save 97% Gas.</h2>
-      <p class="mb-4 text-gray-500 font-thin text-md leading-relaxed">Get hold of your pie almost gas-free by sharing the cost.<br/>Turn your ETH into delicious pie in three easy steps.</p>
-    </div>
-  </section>
+<Modal title="Add Liquidity" backgroundColor="#f3f3f3" bind:this={modalAdd}>
+  <span slot="content">
+    <LiquidityModal
+      token="0x8d1ce361eb68e9e05573443c407d4a3bed23b033"
+      method="single"
+      poolAction="add" />
+  </span>
+</Modal>
+<section class="pt-4 px-4 text-center md:pt-8 lg:pt-12">
+  <div class="w-full max-w-2xl mx-auto">
+    <h2 class="text-xl mt-2 mb-6 leading-tight font-heading">Bake Together, save 97% Gas.</h2>
+    <p class="mb-4 text-gray-500 font-thin text-md leading-relaxed">
+      Get hold of your pie almost gas-free by sharing the cost.<br />Turn your ETH into delicious
+      pie in three easy steps.
+    </p>
+  </div>
+</section>
 
-  <div class="content flex flex-col justify-between spl text-center">
-    <div class="flex flex-col justify-between content-center lg:flex-row">
-      <ProductBox 
-        image={images.depositeth}
-        title="Deposit ETH"
-        description="When at least 10 ETH is deposited the Oven can begin"
-      />
-      
-      <ProductBox 
-        image={images.waitoven}
-        title="Wait"
-        description="Oven will bake when gas prices are affordable, saving everyone money"
-      />
+<div class="content flex flex-col justify-between spl text-center">
+  <div class="flex flex-col justify-between content-center lg:flex-row">
+    <ProductBox class="bg-white"
+      image={images.depositeth}
+      title="Deposit ETH"
+      description="When at least 10 ETH is deposited the Oven can begin" />
 
-      <ProductBox 
+    <ProductBox
+      image={images.waitoven}
+      title="Wait"
+      description="Oven will bake when gas prices are affordable, saving everyone money" />
+
+    <ProductBox
       image={images.sharegascost}
       title="Withdraw Your Pie"
-      description="Once the Pie is baked you can withdraw it to your wallet"
-    />
-    </div>
+      description="Once the Pie is baked you can withdraw it to your wallet" />
+  </div>
 </div>
 
 <section class="pt-12 px-4 text-center">
@@ -230,51 +220,94 @@ import TooltipButton from '../../components/elements/TooltipButton.svelte';
   </table>
 </div> -->
 
-
-<div class="content spl flex flex-col justify-center justify-items-center content-center flex-wrap md:flex-row lg:flex-row">
-{#each ovens as oven}
-<div class="max300 my-10px lg:m-10px p-20px rounded-sm bg-white border-thin">
-    <a class="flex justify-start mb-4 items-center" href={`#/pie/${oven.baking.address}`}>
-        <img class="w-50px h-auto md:w-50px h-auto" src={oven.baking.icon} alt={oven.baking.symbol}/>
+<div
+  class="content spl flex flex-col justify-center justify-items-center content-center flex-wrap md:flex-row lg:flex-row">
+  {#each ovens as oven}
+    <div class="max300 my-10px lg:m-10px p-20px rounded-sm bg-white border-thin">
+      <a class="flex justify-start mb-4 items-center" href={`#/pie/${oven.baking.address}`}>
+        <img
+          class="w-50px h-auto md:w-50px h-auto"
+          src={oven.baking.icon}
+          alt={oven.baking.symbol} />
         <span class="font-bold text-xl text-right ml-2">{oven.baking.symbol}</span>
-    </a>
-    <div class="flex justify-between my-2">
-      <div class="flex items-center "><span class="font-thin text-left mr-2">Minimum reached</span><TooltipButton tooltip="Oven needs 10 ETH and low Gas Price to be activated"><div class="infolink"></div></TooltipButton></div>
-      <div class="font-bold text-right rounded-sm bg-black w-40pc">
-        <div class="px-2 py-1 rounded-sm text-xs bg-gradient-purple text-left text-white fit-content">65%</div>
+      </a>
+      <div class="flex justify-between my-2">
+        <div class="flex items-center ">
+          <span class="font-thin text-left mr-2">Minimum reached</span>
+          <TooltipButton tooltip="Oven needs 10 ETH and low Gas Price to be activated">
+            <div class="infolink" />
+          </TooltipButton>
+        </div>
+        <div class="font-bold text-right rounded-sm bg-black w-40pc">
+          <div
+            class="px-2 py-1 rounded-sm text-xs bg-gradient-purple text-left text-white fit-content">
+            65%
+          </div>
+        </div>
       </div>
+      <div class="flex justify-between my-2">
+        <span class="font-thin text-left">Your ETH in the Oven</span>
+        <span class="font-bold text-right">0.75 ETH</span>
+      </div>
+      <div class="flex justify-between my-2">
+        <span class="font-thin text-left">Pie ready</span>
+        <span class="font-bold text-right">134.50 DEFI++</span>
+      </div>
+      <button
+        on:click={() => {
+          modal.pieAddress = oven.baking.address;
+          modal.ovenAddress = oven.addressOven;
+          modal.deprecated = oven.deprecated;
+          modal.open();
+        }}
+        class="main-cta-ghost m-0 mt-4 rounded-8px p-15px w-full">
+        Deposit / Withdraw
+      </button>
     </div>
-    <div class="flex justify-between my-2">
-      <span class="font-thin text-left">Your ETH in the Oven</span>
-      <span class="font-bold text-right">0.75 ETH</span>
-    </div>
-    <div class="flex justify-between my-2">
-      <span class="font-thin text-left">Pie ready</span>
-      <span class="font-bold text-right">134.50 DEFI++</span>
-    </div>
-    <button on:click={() => {
-        modal.pieAddress = oven.baking.address;
-        modal.ovenAddress = oven.addressOven;
-        modal.deprecated = oven.deprecated;
-        modal.open()
-      }}  class="main-cta-ghost m-0 mt-4 rounded-8px p-15px w-full">
-      Select your Pie
-    </button>
+  {/each}
+</div>
+
+<section class="pt-12 px-4 text-center">
+  <div class="w-full max-w-2xl mx-auto">
+    <h2 class="text-xl mt-2 mb-6 leading-tight font-heading">Can't wait?</h2>
+    <p class="mb-4 text-gray-500 font-thin text-md leading-relaxed">
+      You can always
+      <strong>Buy or Issue</strong>
+      your Pie instantly from the the Pie Page
+    </p>
   </div>
-{/each}
+  <button
+    on:click={modalAdd.open}
+    class="btn m-0 mt-4 rounded-8px p-15px min-w-200px w-96pc lg:w-200px lg:min-w-200px">
+    Select your Pie
+  </button>
+</section>
+
+<section class="pt-12 px-4 text-center">
+  <div class="w-full max-w-2xl mx-auto">
+    <h2 class="text-xl mt-2 mb-6 leading-tight font-heading">How does the Oven work?</h2>
+  </div>
+</section>
+
+<div class="w-100pc flex justify-center">
+<img class="w-100pc h-auto md:w-80pc h-auto"src={images.oven_illustration} alt="Oven Illustration"/>
 </div>
 
 
 
 <section class="pt-12 px-4 text-center">
   <div class="w-full max-w-2xl mx-auto">
-    <h2 class="text-xl mt-2 mb-6 leading-tight font-heading">Can't wait?</h2>
-    <p class="mb-4 text-gray-500 font-thin text-md leading-relaxed">You can always <strong>Buy or Issue</strong> your Pie instantly from the the Pie Page</p>
+    <h2 class="text-xl mt-6 mb-3 leading-tight font-heading">Got more questions?</h2>
   </div>
-  <button on:click={modalAdd.open} class="btn m-0 mt-4 rounded-8px p-15px min-w-200px w-96pc lg:w-200px lg:min-w-200px">
-    Select your Pie
-  </button>
 </section>
 
-
-
+<div class="text-left content spl flex justify-center">
+<Accordion>
+  <AccordionItem title="How does the Oven make me save 97% gas?">How does the Oven make me save 97% gas?</AccordionItem>
+  <AccordionItem title="When will the Oven be triggered?">When will the Oven be triggered?</AccordionItem>
+  <AccordionItem title="Why does it say 100% but it is still not baking?">When will the Oven be triggered?</AccordionItem>
+  <AccordionItem title="How does it take usually to bake a Pie?">How does it take usually to bake a Pie?</AccordionItem>
+  <AccordionItem title="What happens if the ETH I put goes over 10 ETH?">What happens if the ETH I put goes over 10 ETH?</AccordionItem>
+  <AccordionItem title="Is there a time limit to withdraw?">Some serious answer about this question goes here. It can be short, it can be long, it really does not matter at all!</AccordionItem>
+</Accordion>
+</div>
