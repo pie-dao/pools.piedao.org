358571a804dab60a8713d38ff2144b75293cfd93
Nicola Di Marco <ndimarco@amp.energy>
2021-01-20 16:42:04 +0100

    change text color

--- src/components/Change.svelte
+++ src/components/Change.svelte
@@ -10,19 +10,19 @@
   n/a
 {:else}
   {#if value > 0}
-    <div class="flex justify-around items-center fitcontent">
+    <div class="flex justify-around items-center fitcontent text-green">
       <img src={images.icons.ArrowUp} alt="close" class="h-12px mr-2px" />
-      <div class="ml-2px">{value.toFixed(2)} % </div>
+      <div class="ml-2px font-thin">{value.toFixed(2)} % </div>
       <div class="font-thin ml-5px">{#if displayLabel}24h{/if}</div>
     </div>
   {:else if value == 0}
-  <div class="ml-5px">{value.toFixed(2)}%</div>
+  <div class="ml-5px font-thin">{value.toFixed(2)}%</div>
   <div class="font-thin ml-5px">{#if displayLabel}24h{/if}</div>
 
   {:else}
-  <div class="flex justify-around items-center fitcontent">
+  <div class="flex justify-around items-center fitcontent text-red">
         <img src={images.icons.ArrowDown} alt="close" class="h-12px mr-2px" />
-        <div class="ml-2px">{value.toFixed(2)} % </div>
+        <div class="ml-2px font-thin">{value.toFixed(2)} % </div>
         <div class="font-thin ml-5px">{#if displayLabel}24h{/if}</div>
     </div>
   {/if}
