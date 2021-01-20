9f0c5fc6ac1645dfcae6428f63d6804d1d329423
Nicola Di Marco <ndimarco@amp.energy>
2021-01-20 16:33:13 +0100

    alignement in the change component

--- src/components/Change.svelte
+++ src/components/Change.svelte
@@ -11,19 +11,19 @@
 {:else}
   {#if value > 0}
     <div class="flex justify-around items-center fitcontent">
-      <img src={images.icons.ArrowUp} alt="close" class="h-12px mr-5px" />
-      <div class="ml-10px">{value.toFixed(2)} % </div>
-      <div>{#if displayLabel}24h{/if}</div>
+      <img src={images.icons.ArrowUp} alt="close" class="h-12px mr-2px" />
+      <div class="ml-2px">{value.toFixed(2)} % </div>
+      <div class="font-thin ml-5px">{#if displayLabel}24h{/if}</div>
     </div>
   {:else if value == 0}
-  <div class="ml-10px">{value.toFixed(2)}%</div>
-  <div>{#if displayLabel}24h{/if}</div>
+  <div class="ml-5px">{value.toFixed(2)}%</div>
+  <div class="font-thin ml-5px">{#if displayLabel}24h{/if}</div>
 
   {:else}
   <div class="flex justify-around items-center fitcontent">
-        <img src={images.icons.ArrowDown} alt="close" class="h-12px mr-5px" />
-        <div class="ml-10px">{value.toFixed(2)} % </div>
-        <div>{#if displayLabel}24h{/if}</div>
+        <img src={images.icons.ArrowDown} alt="close" class="h-12px mr-2px" />
+        <div class="ml-2px">{value.toFixed(2)} % </div>
+        <div class="font-thin ml-5px">{#if displayLabel}24h{/if}</div>
     </div>
   {/if}
 {/if}
