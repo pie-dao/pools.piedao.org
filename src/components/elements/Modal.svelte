875bf8d2ab6969ef2aa802eebe5f82a455d4cfd2
Nicola Di Marco <ndimarco@amp.energy>
2021-01-25 17:01:14 +0100

    modal max width

--- src/components/elements/Modal.svelte
+++ src/components/elements/Modal.svelte
@@ -19,7 +19,7 @@ let modalIsOpen = false;
 {#if modalIsOpen}
     <div class="genericmodal flex justify-center items-center">
         <div style={ backgroundColor ? `background-color: ${backgroundColor} !important` : "#fff"} 
-             class="flex flex-col justify-start modalcontent w-100pc min-h-100pc p-2 overflow-x-hidden overflow-y-auto lg:max-w-50pc lg:min-w-30pc lg:max-h-80pc lg:min-h-50pc" use:mousedownOutside on:click_outside={() => modalIsOpen = false}>
+             class="flex flex-col justify-start modalcontent w-100pc min-h-100pc p-2 overflow-x-hidden overflow-y-auto lg:max-w-600px lg:min-w-30pc lg:max-h-70pc lg:min-h-50pc" use:mousedownOutside on:click_outside={() => modalIsOpen = false}>
             <div class="flex">
              {#if title}
                 <h1 class="pl-50px text-center text-xl w-100pc"> {title} </h1>
