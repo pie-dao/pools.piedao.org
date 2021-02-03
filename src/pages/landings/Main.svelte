e0adb4e85175920b290c13214bbced7b6c638258
Nicola Di Marco <ndimarco@amp.energy>
2021-02-03 17:03:44 +0100

    newsletter

--- src/pages/landings/Main.svelte
+++ src/pages/landings/Main.svelte
@@ -41,7 +41,7 @@
 
 
 <div class="content font-huge text-left md:text-center" style="padding-bottom: 0!important;">
-  <span class="mt-6 mb-0 md:mb-4 px-6">A decentralised asset manager for tokenised portfolios, with a mission to bring automated wealth creation to everyone with an internet connection.</span>
+  <span class="mt-6 md:mt-12 mb-0 md:mb-4 px-6 italic">"A decentralised asset manager for tokenised portfolios, with a mission to bring automated wealth creation to everyone with an internet connection."</span>
 </div>
 
 
@@ -81,7 +81,7 @@
         </div>
             <div class="p-4 flex flex-col">
             <span class="text-lg">YPIE</span>
-            <span class="font-thin text.base">All the top cap awesome DeFi project you were dreaming of</span>
+            <span class="font-thin text.base">Lending and staking strategies on the Yearn Ecosystem</span>
             </div>
         </a>
         <a href="ciao" class="min-w-80pc md:min-w-31pc md:w-1/3 md:max-w-1/3 mx-4 md:mx-4 my-2 md:my-0 drowpdown-shadow rounded flex flex-col pointer scale">
@@ -90,7 +90,7 @@
           </div>
               <div class="p-4 flex flex-col">
               <span class="text-lg">DeFi+S</span>
-              <span class="font-thin text.base">All the top cap awesome DeFi project you were dreaming of</span>
+              <span class="font-thin text.base">Exposure to lower marketcap projects that have incredible potential for future growth</span>
               </div>
           </a>
           <a href="ciao" class="min-w-80pc md:min-w-31pc md:w-1/3 md:max-w-1/3 mx-4 md:mx-4 my-2 md:my-0 drowpdown-shadow rounded flex flex-col pointer scale">
@@ -99,7 +99,7 @@
             </div>
                 <div class="p-4 flex flex-col">
                 <span class="text-lg">USD++</span>
-                <span class="font-thin text.base">All the top cap awesome DeFi project you were dreaming of</span>
+                <span class="font-thin text.base">A range of centralized and decentralized services produce tokenized US dollars</span>
                 </div>
             </a>
             <a href="ciao" class="min-w-80pc md:min-w-31pc md:w-1/3 md:max-w-1/3 mx-4 md:mx-4 my-2 md:my-0 drowpdown-shadow rounded flex flex-col pointer scale">
@@ -108,7 +108,7 @@
               </div>
                   <div class="p-4 flex flex-col">
                   <span class="text-lg">BTC++</span>
-                  <span class="font-thin text.base">All the top cap awesome DeFi project you were dreaming of</span>
+                  <span class="font-thin text.base">All the benefits of BTC on Ethereum, in the safest way</span>
                   </div>
               </a>
       <div class="block inline-block md:hidden" style="margin-right:2rem!important;">&nbsp;</div>
@@ -119,16 +119,27 @@
 
 <div class="flex rounded gradientbglightgreen py-6 md:py-12 px-4 md:px-6 mx-6 md:mx-40 flex items-center justify-center">
   <div class="flex flex-col md:flex-row max-w-1200px px-2 md:px-8">
-<div class="font-huge text-left w-100pc md:w-1/2">Pies are diversified portfolios of top performing crypto assets</div>
-<ul class="text-left font-thin text-base w-100pc md:w-1/2 md:pl-4 list-outside list-none">
-  <li class="mt-4 md:mt-0">✔️ Carefully handpicked by a decentralised community of economically incentivised talent</li>
-  <li class="mt-4">✔️ Maximised returns through latest-trend active strategies like staking, lending, and yield-farming - completely automated</li>
-  <li class="mt-4">✔️ Low-cost onboarding, saving you 97% of Ethereum gas fees</li>
-  <li class="mt-4">✔️ Architecture that enhances security & minimises risk </li>
-</ul>
+    <img class="w-70px mb-4 block md:hidden" src={images.piechart_illustration} alt="PieDAO chart illustration" />
+    <div class="font-huge text-left w-100pc md:w-1/2">Pies are diversified portfolios of top performing crypto assets</div>
+    <ul class="text-left font-thin text-base w-100pc md:w-1/2 md:pl-4 list-outside list-none">
+      <li class="mt-4 md:mt-0">✔️ Carefully handpicked by a decentralised community of economically incentivised talent</li>
+      <li class="mt-4">✔️ Maximised returns through latest-trend active strategies like staking, lending, and yield-farming - completely automated</li>
+      <li class="mt-4">✔️ Low-cost onboarding, saving you 97% of Ethereum gas fees</li>
+      <li class="mt-4">✔️ Architecture that enhances security & minimises risk </li>
+    </ul>
+  </div>
+</div>
 
+<div class="flex rounded py-6 md:py-12 px-4 md:px-6 mx-6 md:mx-40 flex items-center justify-center">
+  <div class="flex flex-col max-w-1200px px-2 md:px-8">
+    <div class="font-huge text-left md:text-center w-100pc">Subscribe to The Bakery for latest performance updates and community news</div>
+    <div class="flex flex-col md:flex-row nowrap md:items-stretch justify-center mt-4">
+      <input class="newsletter-input-from rounded p-3 w-100pc md:w-2/5" autocomplete="off" autocorrect="off" type="mail"  placeholder="youre@mail.here" minlength="1" maxlength="79" spellcheck="false">
+      <a href="#/pies" class="w-100pc md:w-1/5 md:ml-4 mt-4 md:mt-0"><button class="btnblack m-0 rounded-8px p-15px min-w-200px w-100pc lg:w-200px lg:min-w-200px">Subscribe</button></a>
+    </div>
+    </div>
 </div>
-</div>
+
 
 <div class="videocontainer-landing my-6">
   <video loop muted autoplay poster="https://raw.githubusercontent.com/pie-dao/brand/master/misc/doughvideobg2.jpg" class="bg_video-landing">
