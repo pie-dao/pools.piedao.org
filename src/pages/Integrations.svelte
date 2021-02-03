999282cc542b462c64d7dc28f0c6b3d6c04fd16e
Nicola Di Marco <ndimarco@amp.energy>
2021-02-03 11:01:04 +0100

    fixing text overflow and adding titles

--- src/pages/Integrations.svelte
+++ src/pages/Integrations.svelte
@@ -2,14 +2,12 @@
 import images from "../config/images.json";
 </script>
 
-
-<div class="text-xl leading-10 mb-4 text-center mt-10">
-  PieDAO Integrations
+<div class="content">
+  <div class="justify-start items-start flex-col text-left">
+    <h1 class="text-lg">Integrations</h1>
+    <p class="font-thin">Lots of cool projects where you can buy, sell, farm, track your Pies and more!</p>
+  </div>
 </div>
-<div class="font-thin text-l mb-20px lg:mb-50px text-center">
-  Lots of cool projects where you can buy, sell, farm, track your Pies and more!
-</div>
-
 
 <div class="content flex flex-wrap">
 
@@ -17,7 +15,7 @@ import images from "../config/images.json";
     <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">portfolio tracker</span></div>
     <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F25523c61-f260-4f21-be2f-3e35f2dbd9cd%2F1_z-white_blue-circle.png?table=block&id=b9bfad1e-f7b7-45a7-a2e0-9640f6374ff1&width=250&userId=24e69d32-b625-4b83-ab58-3572798a5901&cache=v2" class="" alt="Project Logo" /></div>
     <div class="text-center font-bold text-lg">Zerion</div>
-    <div class="text-justify font-thin">Zerion is the easiest way to build and manage your entire DeFi portfolio from one place. Discover the world of decentralized finance today.</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">Zerion is the easiest way to build and manage your entire DeFi portfolio from one place. Discover the world of decentralized finance today.</div>
     <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
   </div>
 
@@ -25,7 +23,7 @@ import images from "../config/images.json";
     <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">wallet</span></div>
     <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://images.prismic.io/argentwebsite/313db37e-055d-42ee-9476-a92bda64e61d_logo.svg?auto=format%2Ccompress&fit=max&q=50" class="" alt="Project Logo" /></div>
     <div class="text-center font-bold text-lg">Argent</div>
-    <div class="text-justify font-thin">The most simple and secure smart wallet for crypto.Store and send; borrow, earn interest and invest.The future of money. Own it in a tap.</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">The most simple and secure smart wallet for crypto.Store and send; borrow, earn interest and invest.The future of money. Own it in a tap.</div>
     <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
   </div>
 
@@ -33,7 +31,7 @@ import images from "../config/images.json";
     <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">wallet</span></div>
     <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://pbs.twimg.com/profile_images/1230771588380782595/lC-xrjES_400x400.png" class="" alt="Project Logo" /></div>
     <div class="text-center font-bold text-lg">Rainbow</div>
-    <div class="text-justify font-thin">Rainbow is a fun, simple, and secure Ethereum wallet that makes managing your assets a joy.</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">Rainbow is a fun, simple, and secure Ethereum wallet that makes managing your assets a joy.</div>
     <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
   </div>
 
@@ -41,7 +39,7 @@ import images from "../config/images.json";
     <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">AMM protocol</span></div>
     <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xba100000625a3754423978a60c9317c58a424e3D/logo.png" class="" alt="Project Logo" /></div>
     <div class="text-center font-bold text-lg">Balancer</div>
-    <div class="text-justify font-thin">Balancer is a protocol for programmable liquidity Whitepaper Exchange Generalized AMM Customizable number of assets and weights within a pool</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">Balancer is a protocol for programmable liquidity Whitepaper Exchange Generalized AMM Customizable number of assets and weights within a pool</div>
     <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
   </div>
 
@@ -49,10 +47,60 @@ import images from "../config/images.json";
     <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">ecosystem</span></div>
     <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://gblobscdn.gitbook.com/spaces%2F-MIJtlKyblpY9e3_Ofs_%2Favatar-1606403659805.png?alt=media" class="" alt="Project Logo" /></div>
     <div class="text-center font-bold text-lg">yearn</div>
-    <div class="text-justify font-thin">Yearn Finance is a suite of products in Decentralized Finance (DeFi) that provides lending aggregation, yield generation, and insurance on the Ethereum blockchain. The protocol is maintained by various independent developers and is governed by YFI holders.</div>
+    <div class="text-justify font-thin overflow-hidden h-100px text-ellipsis">Yearn Finance is a suite of products in Decentralized Finance (DeFi) that provides lending aggregation, yield generation, and insurance on the Ethereum blockchain. The protocol is maintained by various independent developers and is governed by YFI holders.</div>
     <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
   </div>
 
+</div>
 
+<div class="content">
+  <div class="justify-start items-start flex-col text-left">
+    <h1 class="text-lg">Ecosystem</h1>
+    <p class="font-thin">Lots of cool projects where you can buy, sell, farm, track your Pies and more!</p>
+  </div>
+</div>
+
+<div class="content flex flex-wrap">
+
+  <div class="flex flex-col w-100pc thinborder md:w-31pc h-370px bg-white rounded p-4 my-2 mx-2 md:my-1pc md:mx-1pc">
+    <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">portfolio tracker</span></div>
+    <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F25523c61-f260-4f21-be2f-3e35f2dbd9cd%2F1_z-white_blue-circle.png?table=block&id=b9bfad1e-f7b7-45a7-a2e0-9640f6374ff1&width=250&userId=24e69d32-b625-4b83-ab58-3572798a5901&cache=v2" class="" alt="Project Logo" /></div>
+    <div class="text-center font-bold text-lg">Zerion</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">Zerion is the easiest way to build and manage your entire DeFi portfolio from one place. Discover the world of decentralized finance today.</div>
+    <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
+  </div>
+
+  <div class="flex flex-col w-100pc thinborder md:w-31pc h-370px bg-white rounded p-4 my-2 mx-2 md:my-1pc md:mx-1pc">
+    <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">wallet</span></div>
+    <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://images.prismic.io/argentwebsite/313db37e-055d-42ee-9476-a92bda64e61d_logo.svg?auto=format%2Ccompress&fit=max&q=50" class="" alt="Project Logo" /></div>
+    <div class="text-center font-bold text-lg">Argent</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">The most simple and secure smart wallet for crypto.Store and send; borrow, earn interest and invest.The future of money. Own it in a tap.</div>
+    <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
+  </div>
+
+  <div class="flex flex-col w-100pc thinborder md:w-31pc h-370px bg-white rounded p-4 my-2 mx-2 md:my-1pc md:mx-1pc">
+    <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">wallet</span></div>
+    <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://pbs.twimg.com/profile_images/1230771588380782595/lC-xrjES_400x400.png" class="" alt="Project Logo" /></div>
+    <div class="text-center font-bold text-lg">Rainbow</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">Rainbow is a fun, simple, and secure Ethereum wallet that makes managing your assets a joy.</div>
+    <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
+  </div>
+
+  <div class="flex flex-col w-100pc thinborder md:w-31pc h-370px bg-white rounded p-4 my-2 mx-2 md:my-1pc md:mx-1pc">
+    <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">AMM protocol</span></div>
+    <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xba100000625a3754423978a60c9317c58a424e3D/logo.png" class="" alt="Project Logo" /></div>
+    <div class="text-center font-bold text-lg">Balancer</div>
+    <div class="text-justify font-thin h-100px text-ellipsis">Balancer is a protocol for programmable liquidity Whitepaper Exchange Generalized AMM Customizable number of assets and weights within a pool</div>
+    <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
+  </div>
+
+  <div class="flex flex-col w-100pc thinborder md:w-31pc h-370px bg-white rounded p-4 my-2 mx-2 md:my-1pc md:mx-1pc">
+    <div class="text-right mb-2"><span class="text-xs font-bold text-white bg-black roundedxs px-2 py-1">ecosystem</span></div>
+    <div class="w-100pc flex items-center justify-center mt-4 mb-2"><img width="70px" height="70px" src="https://gblobscdn.gitbook.com/spaces%2F-MIJtlKyblpY9e3_Ofs_%2Favatar-1606403659805.png?alt=media" class="" alt="Project Logo" /></div>
+    <div class="text-center font-bold text-lg">yearn</div>
+    <div class="text-justify font-thin overflow-hidden h-100px text-ellipsis">Yearn Finance is a suite of products in Decentralized Finance (DeFi) that provides lending aggregation, yield generation, and insurance on the Ethereum blockchain. The protocol is maintained by various independent developers and is governed by YFI holders.</div>
+    <a class="text-center font-thin mt-4 hover:opacity-70" target="_blank" href={`https://medium.com/piedao/dough-tokens-d2479c7ea608`}>Learn more </a>
+  </div>
 
 </div>
+
