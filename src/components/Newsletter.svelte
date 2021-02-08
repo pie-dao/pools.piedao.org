fae73f133e3e67eecfba85d9164a7b4a7473a8ba
Nicola Di Marco <ndimarco@amp.energy>
2021-02-08 21:09:51 +0100

    newsletter input style

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -1,8 +1,9 @@
-<div class="flex rounded py-6 md:py-12 px-4 md:px-6 mx-6 md:mx-40 flex items-center justify-center">
+<div class="flex rounded py-4 md:py-10 px-4 md:px-6 mx-6 md:mx-40 flex items-center justify-center">
   <div class="flex flex-col max-w-1200px px-2 md:px-8">
-    <div class="font-huge text-left md:text-center w-100pc">Subscribe to our newsletter for latest performance updates and community news</div>
+    <div class="text-lg text-center w-100pc block md:hidden leading-7 mb-2">Subscribe to our newsletter for latest performance updates and community news</div>
+    <div class="font-huge text-center w-100pc hidden md:block">Subscribe to our newsletter for latest performance updates and community news</div>
     <div class="flex flex-col md:flex-row nowrap md:items-stretch justify-center mt-4">
-      <input class="newsletter-input-from rounded p-3 w-100pc md:w-2/5" autocomplete="off" autocorrect="off" type="mail"  placeholder="youre@mail.here" minlength="1" maxlength="79" spellcheck="false">
+      <input class="newsletter-input-from py-3 px-4 w-100pc md:w-2/5" autocomplete="off" autocorrect="off" type="mail"  placeholder="youre@mail.here" minlength="1" maxlength="79" spellcheck="false">
       <a href="#/pies" class="w-100pc md:w-1/5 md:ml-4 mt-4 md:mt-0"><button class="btnblack m-0 rounded-8px p-15px min-w-200px w-100pc lg:w-200px lg:min-w-200px">Subscribe</button></a>
     </div>
     </div>
