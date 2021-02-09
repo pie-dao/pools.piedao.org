bd1191fe4dd89067a913ebaec6377d4f6e1a408d
Nicola Di Marco <ndimarco@amp.energy>
2021-02-09 17:19:12 +0100

    title

--- src/components/FeaturedIn.svelte
+++ src/components/FeaturedIn.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="content flex items-center flex-col w-100pc px-6 md:px-0 justify-between spl text-center md:w-80pc">
-  <div class="text-black text-base font-thin mb-10px">Featured in</div>
+  <div class="text-black text-base font-thin mb-10px">Press & Integrations</div>
     <div class="flex justify-center">
         <img class="w-80pc h-auto hidden md:block" src={images.press} alt="PieDAO Featured In" />
         <img class="w-100pc h-100pc block md:hidden" src={images.pressmobile} alt="PieDAO Featured In" />
