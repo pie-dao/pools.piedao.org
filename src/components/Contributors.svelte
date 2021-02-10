cc61b4c44bb23d1e64a19e7ae1bfa77e82f66ca7
Nicola Di Marco <ndimarco@amp.energy>
2021-02-10 15:54:22 +0100

    styling contributors

--- src/components/Contributors.svelte
+++ src/components/Contributors.svelte
@@ -7,13 +7,13 @@
   <div class="flex flex-col max-w-1200px px-2 md:px-8">
     <div class="text-lg text-center w-100pc block md:hidden leading-7 mb-2">If you want to go fast, go alone. If you want to go far, DAO it together.</div>
     <div class="font-huge text-center w-100pc hidden md:block">If you want to go fast, go alone.<br/>If you want to go far, DAO it together.</div>
-    <div class="flex flex-col md:flex-row items-center justify-between mt-2 md:mt-8">
-      <div class="font-base font-thin text-left w-100pc">Our fast growing community inlcudes top founders and developers of DeFi, join us!</div>
-      <a target="_blank" href="http://discord.link/PieDAO"><div class="bg-lightgrey text-xs font-bold max-content py-2 px-4 rounded-xl thinborder-purple pointer flex mt-2 md:mt-0">+ 4682 <span class="font-thin ml-2">members</span></div></a>
+    <div class="flex flex-col md:flex-row items-center justify-between mt-2 md:mt-8 self-center">
+      <div class="font-base font-thin text-left w-100pc ">Our fast growing community inlcudes top founders and developers of DeFi, join us!</div>
+      <a target="_blank" href="http://discord.link/PieDAO"><div class="bg-lightgrey text-xs font-bold max-content py-2 px-4 rounded-xl thinborder-purple pointer flex ml-4 mt-2 md:mt-0">+ 4682 <span class="font-thin ml-2">members</span></div></a>
     </div>
   </div>
 
-  <div class="flex max-w-100pc md:max-w-1200px px-2 md:px-8 mt-4 flex-wrap justify-start">
+  <div class="flex max-w-100pc md:max-w-1200px px-2 md:px-8 mt-4 flex-wrap justify-start self-center">
       
     <a href="https://twitter.com/kaiynne" target="_blank" class="md:min-w-200px md:w-200px w-100pc flex items-center bg-lightgrey thinborder rounded p-2 thinborder-purple pointer mr-2 md:mt-2 hidden md:flex md:visible">
         <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1170881713926365184/gI59qCJT_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
@@ -24,34 +24,34 @@
       </a>
 
       <a href="https://twitter.com/kaiynne" target="_blank" class="md:min-w-200px md:w-200px w-100pc flex items-center bg-lightgrey thinborder rounded p-2 thinborder-purple pointer mr-2 mt-2 hidden md:flex md:visible">
-        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1170881713926365184/gI59qCJT_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1052294100613881856/dCB_6FUB_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
         <div class="ml-2 flex flex-col items-start">
-          <span class="text-left font-bold text-base">Kain.eth</span>
-          <span class="text-left text-sm font-thin">Synthetix</span>
+          <span class="text-left font-bold text-base">Linda Xie</span>
+          <span class="text-left text-sm font-thin">0xproject</span>
         </div>
       </a>
 
       <a href="https://twitter.com/kaiynne" target="_blank" class="md:min-w-200px w-200px flex items-center bg-lightgrey thinborder rounded p-2 thinborder-purple pointer mr-2 mt-2 hidden md:flex md:visible">
-        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1170881713926365184/gI59qCJT_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1297876175365308417/MKrJLWrh_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
         <div class="ml-2 flex flex-col items-start">
-          <span class="text-left font-bold text-base">Kain.eth</span>
-          <span class="text-left text-sm font-thin">Synthetix</span>
+          <span class="text-left font-bold text-base">tracheroysz.eth</span>
+          <span class="text-left text-sm font-thin">Yearn Finance</span>
         </div>
       </a>
 
       <a href="https://twitter.com/kaiynne" target="_blank" class="md:min-w-200px w-200px flex items-center bg-lightgrey thinborder rounded p-2 thinborder-purple pointer mr-2 mt-2 hidden md:flex md:visible">
-        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1170881713926365184/gI59qCJT_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/845097986912731136/VSF3gqwv_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
         <div class="ml-2 flex flex-col items-start">
-          <span class="text-left font-bold text-base">Kain.eth</span>
-          <span class="text-left text-sm font-thin">Synthetix</span>
+          <span class="text-left font-bold text-base">Thatgirl.eth</span>
+          <span class="text-left text-sm font-thin">Go go go ethereum</span>
         </div>
       </a>
 
       <a href="https://twitter.com/kaiynne" target="_blank" class="md:min-w-200px w-200px flex items-center bg-lightgrey thinborder rounded p-2 thinborder-purple pointer mr-2 mt-2 hidden md:flex md:visible">
-        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1170881713926365184/gI59qCJT_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle" style="background: url(https://pbs.twimg.com/profile_images/1167538537086017536/gGIg7ehY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
         <div class="ml-2 flex flex-col items-start">
-          <span class="text-left font-bold text-base">Kain.eth</span>
-          <span class="text-left text-sm font-thin">Synthetix</span>
+          <span class="text-left font-bold text-base">Amico mio</span>
+          <span class="text-left text-sm font-thin">Apple</span>
         </div>
       </a>
 
@@ -78,22 +78,22 @@
         <div class="w-60px h-60px circle mr-2 hidden md:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
       <a href="https://twitter.com/Alexintosh" target="_blank">
-        <div class="w-60px h-60px circle mr-2 hidden md:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle mr-2 hidden md:block" style="background: url(https://pbs.twimg.com/profile_images/1279616178235858948/iXXJ-jh9_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
       <a href="https://twitter.com/Alexintosh" target="_blank">
-        <div class="w-60px h-60px circle mr-2 hidden md:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle mr-2 hidden md:block" style="background: url(https://pbs.twimg.com/profile_images/1126472830873370624/33dNeSjC_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
       <a href="https://twitter.com/Alexintosh" target="_blank">
-        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1253165670935773185/SkSoEQL3_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
       <a href="https://twitter.com/Alexintosh" target="_blank">
-        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1311779549693317121/0GpBW9T-_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
       <a href="https://twitter.com/Alexintosh" target="_blank">
-        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1176132107065798656/JJYULS9K_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
       <a href="https://twitter.com/Alexintosh" target="_blank">
-        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1225182358955069443/iDGzgOTY_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
+        <div class="w-60px h-60px circle mr-2 hidden lg:block" style="background: url(https://pbs.twimg.com/profile_images/1288307207759523840/JQrKnuaK_400x400.jpg) no-repeat center center; background-size: cover!important;"></div>
       </a>
 
 
