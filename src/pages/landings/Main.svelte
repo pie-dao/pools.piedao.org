ac7c2d4b4f54dd06602dd3db5e8486b17e1e3d34
Nicola Di Marco <ndimarco@amp.energy>
2021-02-15 17:38:21 +0100

    copy

--- src/pages/landings/Main.svelte
+++ src/pages/landings/Main.svelte
@@ -65,21 +65,21 @@
     <div class="max-w-1200px flex flex-col items-center justify-center py-0 md:py-8 px-6">
     <!-- <img class="w-90pc block md:hidden mb-2"  src={images.herolandingmobile} alt="PieDAO Hero" /> -->
     <img class="md:w-90pc lg:w-80pc hidden md:block"  src={images.herolanding} alt="PieDAO Hero" />
-    <div class="text-black font-bold text-center font-hero linear-wipe">Automated<br class="hidden md:block lg:block"/>  wealth creation</div>
-    <div class="text-lg text-black font-thin italic text-center mt-1">“Decentralized Finance Made Easy”</div>
+    <div class="text-black font-bold text-center font-hero linear-wipe">Automated<br class="hidden md:block lg:block"/> wealth creation</div>
+    <div class="text-lg text-black font-thin italic text-center mt-1">Decentralized Finance <br class="block md:hidden lg:hidden"/>Made Easy</div>
     <a href="#/pies"><button class="btnbig text-white m-0 mt-8 rounded-8px p-15px min-w-200px w-100pc lg:w-200px lg:min-w-200px">Get started</button></a>
     <div class="w-100pc flex flex-col md:flex-row items-stretch text-left mt-6 mb-0 md:mt-10 md:mb-6 text-black font-bold text-base">
       <div class="flex w-100pc justify-start items-center md:justify-center mt-2 md:mx-3 md:mt-0 md:w-1/3 bg-black-alpha rounded py-4 px-4">
         <img class="w-40px" src={images.heroicon2} alt="PieDAO Hero" />
-        <span class="ml-4 leading-5">Set and forget strategies<br class="hidden md:block" />for maximum returns</span>
+        <span class="ml-4 leading-5">Set and forget strategies, <br class="hidden md:block" />maximum returns</span>
       </div>
       <div class="flex w-100pc justify-start items-center md:justify-center mt-2 md:mx-3 md:mt-0 md:w-1/3 bg-black-alpha rounded py-4 px-4">
         <img class="w-40px" src={images.heroicon1} alt="PieDAO Hero" />
-        <span class="ml-4 leading-5">Diversified exposure to<br class="hidden md:block" />crème de la crème in crypto</span>  
+        <span class="ml-4 leading-5">Diversified exposure to <br class="hidden md:block" />the best of crypto</span>  
       </div>
       <div class="flex w-100pc justify-start items-center md:justify-center mt-2 md:mx-3 md:mt-0 md:w-1/3 bg-black-alpha rounded py-4 px-4">
         <img class="w-40px" src={images.heroicon3} alt="PieDAO Hero" />
-        <span class="ml-4 leading-5">Secure, transparent,<br class="hidden md:block" />and open-source</span>
+        <span class="ml-4 leading-5">Secure, transparent, <br class="hidden md:block" />and open-source</span>
       </div>
     </div>
   </div>
@@ -87,8 +87,8 @@
 
 
 <div class="content text-left md:text-center" style="padding-bottom: 0!important;">
-  <span class="mt-6 mb-0 px-6 md:px-8 italic text-lg block md:hidden leading-7">"A decentralised asset manager for tokenised portfolios, with a mission to bring automated wealth creation to everyone with an internet connection."</span>
-  <span class="mt-12 mb-4 px-8 italic font-huge hidden md:block">"A decentralised asset manager for tokenised portfolios, with a mission to bring automated wealth creation to everyone with an internet connection."</span>
+  <span class="mt-6 mb-0 px-6 md:px-8 italic text-lg block md:hidden leading-7">"A decentralized asset manager for tokenized portfolios, with a mission to bring automated wealth creation to everyone with an internet connection."</span>
+  <span class="mt-12 mb-4 px-8 italic font-huge hidden md:block">"A decentralized asset manager for tokenized portfolios, with a mission to bring automated wealth creation to everyone with an internet connection."</span>
 </div>
 
 
@@ -159,10 +159,10 @@
     <div class="text-lg text-left w-100pc block md:hidden leading-7 mb-2">Pies are diversified portfolios of top performing crypto assets</div>
     <div class="font-huge text-left w-1/2 pr-2 hidden md:block">Pies are diversified portfolios of top performing crypto assets</div>
     <ul class="text-left font-thin font-base w-100pc md:w-1/2 md:pl-4 list-outside list-none">
-      <li class="mt-3 md:mt-0">✔️ Carefully handpicked by a decentralised community of economically incentivised talent</li>
-      <li class="mt-3">✔️ Maximised returns through latest-trend active strategies like staking, lending, and yield-farming - completely automated</li>
-      <li class="mt-3">✔️ Low-cost onboarding, saving you 97% of Ethereum gas fees</li>
-      <li class="mt-3">✔️ Architecture that enhances security & minimises risk </li>
+      <li class="mt-3 md:mt-0">✔️ Carefully handpicked by a decentralized community of crypto natives who live and breathe DEFI</li>
+      <li class="mt-3">✔️ Maximize returns with active yield-generating strategies under the hood</li>
+      <li class="mt-3">✔️ Accessible. Save 97% of the minting gas costs by using the community Oven</li>
+      <li class="mt-3">✔️ Secure architecture and fully audited contracts </li>
     </ul>
   </div>
 </div>
