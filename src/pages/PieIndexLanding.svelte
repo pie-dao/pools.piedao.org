e67fc4f8ebc25aa6e9380d1ae8e424abd0f58961
Alessio Delmonti <alessio.d@gmail.com>
2021-02-18 08:14:28 +0100

    Fix nav value mobile

--- src/pages/PieIndexLanding.svelte
+++ src/pages/PieIndexLanding.svelte
@@ -318,7 +318,7 @@
   <div class="md:hidden w-100pc flex flex-col rounded-sm border-grey my-2 p-2">
     <div class="w-100pc flex justify-between items-center">
       <div class="font-bold text-left text-pink"><a on:click={() => { modalinfo.open() }} class="cursor-pointer flex" role="menuitem"><span>NAV </span><img src={images.InfoIcon} class="ml-1" alt="info" width="16px" /></div>
-        <div class="font-bold text-right">{nav}</div>
+        <div class="font-bold text-right">{getNav}</div>
     </div>
     <div class="w-100pc flex justify-between items-center">
       <div class="font-thin text-left">{getSpread.label}</div>
