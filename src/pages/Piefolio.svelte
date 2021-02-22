222f278c240b5c4cdd55fc7410ba4cf807e4c0c2
Nicola Di Marco <ndimarco@amp.energy>
2021-02-22 11:32:00 +0100

    layout

---
+++ src/pages/Piefolio.svelte
@@ -0,0 +1,17 @@
+<script>
+  import images from "../config/images.json";
+</script>
+
+
+<div class="content">
+
+  <div class="flex flex-col w-62pc bg-grey"> 5/8</div>
+  <div class="flex flex-col w-38pc bg-lightgrey"> 3/8</div>
+
+</div>
+
+ 
+
+
+
+  
