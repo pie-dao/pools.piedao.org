fb92e1e8ab5e7ed74035153ccc8c75a16faef66f
Nicola Di Marco <ndimarco@amp.energy>
2021-02-22 13:21:26 +0100

    your holdings

---
+++ src/components/Holdings.svelte
@@ -0,0 +1,48 @@
+<script>
+  import images from "../config/images.json";
+</script>
+
+<div class="bg-purple rounded-xl text-white py-8 px-6">
+  <div class="font-huge">Your holdings</div>
+  
+  <a class="flex mt-4" href="token">
+    <img width="60px" height="60px" class="mr-2" src={images.doughtoken} alt="token name" />
+    <div class="flex flex-col justify-around">
+      <span class="flex items-center"><span class="text-lg leading-6">DOUGH</span><span class="bg-black opacity-60 px-5px py-1px rounded-sm text-xs ml-2">55.30%</span></span>
+      <span class="text-sm font-thin">1,450.20 DOUGH • $12,823.92</span>
+    </div>
+    <div class="flex flex-col justify-around text-right ml-auto font-thin">
+      <span class="text-lg leading-6">$1.69</span>
+      <span class="text-sm px-1 text-green">+ 5.37%</span>
+    </div>
+  </a>
+
+  <a class="flex mt-4" href="token">
+    <img width="60px" height="60px" class="mr-2" src={images.bcp} alt="token name" />
+    <div class="flex flex-col justify-around">
+      <span class="flex items-center"><span class="text-lg leading-6">BCP</span><span class="bg-black opacity-60 px-5px py-1px rounded-sm text-xs ml-2">27.00%</span></span>
+      <span class="text-sm font-thin">1,450.20 BCP • $5,748.21</span>
+    </div>
+    <div class="flex flex-col justify-around text-right ml-auto font-thin">
+      <span class="text-lg leading-6">$5.38</span>
+      <span class="text-sm px-1 text-green">+ 12.08%</span>
+    </div>
+  </a>
+
+  <a class="flex mt-4" href="token">
+    <img width="60px" height="60px" class="mr-2" src={images.defiplusl} alt="token name" />
+    <div class="flex flex-col justify-around">
+      <span class="flex items-center"><span class="text-lg leading-6">DEFI+L</span><span class="bg-black opacity-60 px-5px py-1px rounded-sm text-xs ml-2">22.70%</span></span>
+      <span class="text-sm font-thin">1,450.20 DEFI+L • $3,564.28</span>
+    </div>
+    <div class="flex flex-col justify-around text-right ml-auto font-thin">
+      <span class="text-lg leading-6">$5.22</span>
+      <span class="text-sm px-1 text-green">+ 2.50%</span>
+    </div>
+  </a>
+
+  
+  
+
+</div>
+
--- src/pages/Piefolio.svelte
+++ src/pages/Piefolio.svelte
@@ -1,11 +1,16 @@
 <script>
   import images from "../config/images.json";
+  import Holdings from "../components/Holdings.svelte";
 </script>
 
 
 <div class="content">
 
-  <div class="flex flex-col w-62pc bg-grey"> 5/8</div>
+  <div class="flex flex-col w-60pc mr-2pc">
+    <Holdings />
+  </div>
+
+
   <div class="flex flex-col w-38pc bg-lightgrey"> 3/8</div>
 
 </div>
