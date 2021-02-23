6fba3378cc6f28455f07c11811ebe143778fc743
Nicola Di Marco <ndimarco@amp.energy>
2021-02-23 16:49:54 +0100

    main landing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import "./styles.css";
-  
+  import AOS from 'aos';
+  import 'aos/dist/aos.css'; 
   import Analytics from "./components/elements/Analytics.svelte";
   import Footer from "./components/Footer.svelte";
   import Header from "./components/Header.svelte";
-
   import { currentRoute } from "./stores/routes.js";
+
+  AOS.init();
 </script>
 
 <section id="App">
