583542fd28f1389fe8d81224f712c835f4791dc6
Nicola Di Marco <ndimarco@amp.energy>
2021-03-02 20:53:05 +0100

    modal

---
+++ src/components/elements/ModalFarm.svelte
@@ -0,0 +1,31 @@
+<script>
+import {mousedownOutside} from '../../helpers/mousedownOutside.js';
+import images from '../../config/images.json';
+
+export let backgroundColor;
+export let title;
+
+export const open = () => {
+    modalIsOpen = true;
+}
+
+export const close = () => {
+    modalIsOpen = false;
+}
+
+let modalIsOpen = false;
+</script>
+
+{#if modalIsOpen}
+    <div class="genericmodal flex justify-center items-center">
+        <div class="flex flex-col justify-start p-1px cardbordergradient w-80pc min-h-100pc overflow-x-hidden overflow-y-auto lg:max-w-60pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:mousedownOutside on:click_outside={() => modalIsOpen = false}>
+
+            <button on:click={close} class="w-30px h-30px ml-auto relative top-20px right-20px">
+                <img src={images.closebutton} alt="closebutton" />
+            </button>
+            <slot name="content">
+                
+            </slot>
+        </div>
+    </div>
+{/if}
--- src/pages/LPStaking.svelte
+++ src/pages/LPStaking.svelte
@@ -22,8 +22,7 @@
   import Farming from '../components/piefolio/Farming.svelte';
   import Accordion from '../components/elements/Accordion.svelte';
   import AccordionGroup from '../components/elements/AccordionGroup.svelte';
-  import ModalBig from '../components/elements/ModalBig.svelte';
-
+  import ModalFarm from '../components/elements/ModalFarm.svelte';
 
   import {
     amountFormatter,
@@ -52,6 +51,7 @@
   } from '../stores/eth.js';
 
   import incentivizedPools from '../config/farmingConf.js';
+  import About from './landings/about.svelte';
 
   const isAddress = (thing) => thing && ethers.utils.isHexString(thing) && thing.length === 42;
 
@@ -542,10 +542,9 @@
     }
     pool = _pool;
   };
-  
+
   let modalinfo;
   let modal;
-
 </script>
 
 <Meta
@@ -556,68 +555,19 @@
   }}
 />
 
-<div class="cardbordergradient">
-<ModalBig title="DOUGH / ETH" backgroundColor="#ffffff" bind:this="{modalinfo}">
+<ModalFarm bind:this={modalinfo}>
 
 
-  <a class="flex mt-4 w-100pc rounded-xl bg-white p-4 pointer" href="xx">
-    <div class="mr-4 flex items-center">
-      <img
-        class="z-10"
-        width="60px"
-        height="60px"
-        src={images.doughtoken}
-        alt="token name"
-      />
-      <img
-        class="-ml-30px"
-        width="60px"
-        height="60px"
-        src={images.logos.eth}
-        alt="token name"
-      />
-    </div>
-    <div class="flex flex-col justify-around">
-      <span class="flex items-center"
-        ><span class="text-lg leading-6">DOUGH / ETH</span><span
-          class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs ml-2"
-          >55.30% APY</span
-        ></span
-      >
-      <span class="text-sm font-thin">40% Liquid - 60% Escrowed</span>
-    </div>
-    <div class="flex flex-col justify-around text-right ml-auto font-thin">
-      <span class="text-lg leading-6">Balancer</span>
-      <span class="text-sm px-1 text-grey">Tot 166.345 BPT Staked</span>
-    </div>
-  </a>
-
-
-  <span slot="content" class="p-12 font-thin">
-
-
-    <span class="flex mt-4 w-100pc rounded-xl bg-white p-4">
+  <span slot="content" class="py-0 px-12 font-thin">
+    <span class="flex mt-4 w-100pc">
       <div class="mr-4 flex items-center">
-        <img
-          class="z-10"
-          width="60px"
-          height="60px"
-          src={images.doughtoken}
-          alt="token name"
-        />
-        <img
-          class="-ml-30px"
-          width="60px"
-          height="60px"
-          src={images.logos.eth}
-          alt="token name"
-        />
+        <img class="z-10" width="60px" height="60px" src={images.doughtoken} alt="token name" />
+        <img class="-ml-30px" width="60px" height="60px" src={images.logos.eth} alt="token name" />
       </div>
       <div class="flex flex-col justify-around">
         <span class="flex items-center"
-          ><span class="text-lg leading-6">DOUGH / ETH</span><span
-            class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs ml-2"
-            >55.30% APY</span
+          ><span class="text-lg leading-6 font-bold">DOUGH / ETH</span><span
+            class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs ml-2 font-bold">55.30% APY</span
           ></span
         >
         <span class="text-sm font-thin">40% Liquid - 60% Escrowed</span>
@@ -628,17 +578,150 @@
       </div>
     </span>
 
-    <strong>NAV</strong><br/>
-    The net asset value (NAV) of a Pie represents the market value of each share’s portion of the Pie's underlying assets.<br/>
-    The NAV is determined by adding up the value of all assets in the Pie and then dividing that value by the number of outstanding shares in the Pie.<br/><br/>
+    <span class="w-100pc">
+      <div
+        class="flex flex-col nowrap w-100pc swap-from border rounded-20px border-grey p-16px bg-white mt-8">
+        <div class="flex items-center justify-between">
+          <div class="flex nowrap intems-center p-1 font-thin">Amount to Stake</div>
+          <div
+            class="right text-white font-bold text-xs py-1px text-center align-right float-right rounded"
+          >
+            <button class="oven-withdraw-button">100%</button>
+          </div>
+        </div>
+        <div class="flex nowrap items-center p-1">
+          <input
+            class="swap-input-from"
+            inputmode="decimal"
+            autocomplete="off"
+            autocorrect="off"
+            type="number"
+            pattern="^[0-9]*[.]?[0-9]*$"
+            placeholder="0.0"
+            minlength="1"
+            maxlength="79"
+            spellcheck="false"
+          />
+          <div class="h-32px flex items-center pointer">
+            <img
+              class="token-icon w-30px h-30px"
+              src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png"
+              alt="ETH"
+            />
+            <span class="py-2px px-4px">BPT</span>
+          </div>
+        </div>
+      </div>
+      <button class="btn clear stake-button mt-10px rounded-20px p-15px w-100pc">Stake</button>
+    </span>
 
-    <strong>Price</strong><br/>
-    The Pie's market price is the price at which shares in the Pies can be bought or sold on the exchanges.<br/>
-    The market price can fluctuate throughout the day as buyers and sellers interact with one another and trade.<br/>
-    For this reason, at times the price can differ from the NAV, making it more convenient to buy or mint according to market fluctuations.
+    <span class="flex mt-4 justify-between">
+      <span class="mr-1 w-1/2">
+        <div
+          class="flex flex-col nowrap w-100pc swap-from border rounded-20px border-grey p-16px bg-white"
+        >
+          <div class="flex items-center justify-between">
+            <div class="flex nowrap intems-center p-1 font-thin">Amount to Claim</div>
+            <div
+              class="right text-white font-bold text-xs py-1px text-center align-right float-right rounded"
+            >
+              <button class="oven-withdraw-button">100%</button>
+            </div>
+          </div>
+          <div class="flex nowrap items-center p-1">
+            <input
+              class="swap-input-from"
+              inputmode="decimal"
+              autocomplete="off"
+              autocorrect="off"
+              type="number"
+              pattern="^[0-9]*[.]?[0-9]*$"
+              placeholder="0.0"
+              minlength="1"
+              maxlength="79"
+              spellcheck="false"
+            />
+            <div class="h-32px flex items-center pointer">
+              <img
+                class="token-icon w-30px h-30px"
+                src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png"
+                alt="ETH"
+              />
+              <span class="py-2px px-4px">DOUGH</span>
+            </div>
+          </div>
+        </div>
+        <button class="clear farm-button-ghost mt-10px rounded-20px p-15px w-100pc border-grey">Claim</button>
+      </span>
+
+      <span class="ml-1 w-1/2">
+        <div
+          class="flex flex-col nowrap w-100pc swap-from border rounded-20px border-grey p-16px bg-white"
+        >
+          <div class="flex items-center justify-between">
+            <div class="flex nowrap intems-center p-1 font-thin">Amount to Unstake</div>
+            <div
+              class="right text-white font-bold text-xs py-1px text-center align-right float-right rounded"
+            >
+              <button class="oven-withdraw-button">100%</button>
+            </div>
+          </div>
+          <div class="flex nowrap items-center p-1">
+            <input
+              class="swap-input-from"
+              inputmode="decimal"
+              autocomplete="off"
+              autocorrect="off"
+              type="number"
+              pattern="^[0-9]*[.]?[0-9]*$"
+              placeholder="0.0"
+              minlength="1"
+              maxlength="79"
+              spellcheck="false"
+            />
+            <div class="h-32px flex items-center pointer">
+              <img
+                class="token-icon w-30px h-30px"
+                src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png"
+                alt="ETH"
+              />
+              <span class="py-2px px-4px">BPT</span>
+            </div>
+          </div>
+        </div>
+        <button class="clear farm-button-ghost mt-10px rounded-20px p-15px w-100pc border-grey">Unstake</button>
+      </span>
+
+    </span>
+
+    <div class="info-box mt-4 mb-8">
+      <h1 class="text-xl text-left font-bold">Info</h1>
+      <p>
+        ℹ️ <strong>YPIE/ETH</strong> Staking Rewards - the pool will keep receiving 20000 DOUGH as nominal
+        weekly reward distributed to LPs, of which 20% distributed liquid along the week 80% escrowed
+        within the staking contract, and subject to 52 weeks vesting from the moment they will be claimed.
+      </p>
+      <br />
+      <p>There are total of : <strong>2740.9505 UNI-V2 </strong>.</p>
+      <p>There are total : <strong>2579.4828 UNI-V2</strong> staked in the Staking contract.</p>
+      <p>
+        You are staking : <strong>0%</strong>
+        of the pool = [0 YPIE, 0 ETH] = $ 0.00
+      </p>
+      <br />
+      <p>
+        You can add liquidity to the 🦄 Uniswap pool to get UNI-V2 tokens
+        <a
+          target="_blank"
+          href="https://info.uniswap.org/pair/0xdf5096804705d135656b50b62f9ee13041253d97">HERE</a
+        >
+      </p>
+      <p>Weekly rewards for this pool are <strong>20000 DOUGH</strong></p>
+      <p><a href="#/swap">Buy YPIE !</a></p>
+      <p><a href="#/swap">Buy DOUGH!</a></p>
+    </div>
   </span>
-</ModalBig>
-</div>
+</ModalFarm>
 
 <div class="content">
   <div class="flex items-start px-20px w-100pc max-w-1280px">
@@ -650,9 +733,12 @@
           >
             <div class="w-100pc font-huge text-left">Farm Pools</div>
 
-            <a class="flex mt-4 w-100pc rounded-xl bg-white p-4 pointer" on:click={() => {
-              modalinfo.open()
-            }}>
+            <a
+              class="flex mt-4 w-100pc rounded-xl bg-white p-4 pointer"
+              on:click={() => {
+                modalinfo.open();
+              }}
+            >
               <div class="mr-4 flex items-center">
                 <img
                   class="z-10"
