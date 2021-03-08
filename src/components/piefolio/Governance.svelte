ec56414694d892cc6582ca0ebaee8545f9f3a509
Alessio Delmonti <alessio.d@gmail.com>
2021-03-08 16:34:50 +0000

    Governance component

--- src/components/piefolio/Governance.svelte
+++ src/components/piefolio/Governance.svelte
@@ -1,42 +1,64 @@
 <script>
+  import moment from 'moment';
+  import { onMount } from 'svelte';
+  import filter from "lodash/filter";
   import images from "../../config/images.json";
+
+  $: isLoading = false;
+  $: initialized = false;
+  $: proposals = [];
+
+  const fetchProposals = async () => {
+    const response = await fetch(`https://hub.snapshot.page/api/piedao/proposals`);
+    const result = await response.json();
+
+    var now = moment();
+    let activeProposals = filter(Object.entries(result), ([key, component]) => {
+      if( now.isBetween(moment.unix(component.msg.payload.start), moment.unix(component.msg.payload.end) ) ) {
+        return true;
+      }
+      return false;
+    });
+    
+    if(activeProposals.length > 0) {
+      return activeProposals.map( o => o[1]);
+    }
+    return [];
+  }
+
+  onMount(async () => {
+    if(initialized) return;
+    proposals = await fetchProposals();
+    console.log('proposals', proposals);
+    initialized = true;
+  });
 </script>
 
 <span class="-mt-20px">
   <a class="" href="https://snapshot.page/#/piedao" target="_blank"><img width="20px" height="20px" class="ml-auto relative top-40px right-20px" src={images.extLink} alt="external link icon" /></a>
 
+  
   <div class="bg-lightpink rounded-xl text-black py-8 px-6">
-    <div class="font-huge text-center">Governance</div>
-    
-    <a href="link" target="_blank">
-      <div class="w-100pc rounded-xl bg-white p-6 mt-6">
-        <div class="font-bold leading-5 block break-all">PIP-36 Formalize budget for operational marketing activities</div>
-        <span class="flex items-center mt-1">
-          <span class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs mr-2">Snapshot</span>
-          <span class="text-base leading-6 font-thin opacity-60">Ends Dec 5, 10:00am</span>
-        </span>
-      </div>
-    </a>
-
-    <a href="link" target="_blank">
-      <div class="w-100pc rounded-xl bg-white p-6 mt-4">
-        <div class="font-bold leading-5 block break-all">DeFi+S rebalancing - IPFS</div>
-        <span class="flex items-center mt-1">
-          <span class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs mr-2">Aragon</span>
-          <span class="text-base leading-6 font-thin opacity-60">Ends Dec 7, 12:00am</span>
-        </span>
-      </div>
-    </a>
-
-    <a href="link" target="_blank">
-      <div class="w-100pc rounded-xl bg-white p-6 mt-4">
-        <div class="font-bold leading-5 block break-all">PIP-37 FEnable SUSHI / YFI staking on YPIE</div>
-        <span class="flex items-center mt-1">
-          <span class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs mr-2">Snapshot</span>
-          <span class="text-base leading-6 font-thin opacity-60">Ends Dec 7, 09:00am</span>
-        </span>
-      </div>
-    </a>
+    {#if !initialized}
+      Loading...
+    {:else if initialized && proposals.length === 0}
+      <div class="font-huge text-center">Governance</div>
+      No active proposals at the moment.
+    {:else}
+      <div class="font-huge text-center">Governance</div>
+      
+      {#each proposals as p}
+        <a href={`https://snapshot.page/#/piedao/proposal/${p.authorIpfsHash}`} target="_blank">
+          <div class="w-100pc rounded-xl bg-white p-6 mt-6">
+            <div class="font-bold leading-5 block break-all">{p.msg.payload.name}</div>
+            <span class="flex items-center mt-1">
+              <span class="bg-darkpurple text-white px-5px py-1px roundedxs text-xs mr-2">Snapshot</span>
+              <span class="text-base leading-6 font-thin opacity-60">Ends on: {moment(moment.unix(p.msg.payload.end)).format('MMM Do YYYY')}</span>
+            </span>
+          </div>
+        </a>
+      {/each}
+    {/if}
     
   </div>
 </span>
