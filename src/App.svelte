50d33dc7ad33f6f90251fc3b0e273ccd59ce75ff
Nicola Di Marco <ndimarco@amp.energy>
2021-03-22 14:18:22 +0100

    correct video

--- src/App.svelte
+++ src/App.svelte
@@ -16,8 +16,7 @@
 <ModalVideo backgroundColor="">
   <span slot="content">
     <div class="videoWrapper">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/2AT6LqYjj4s?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    </div>
-  </span>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/fXbuJ7h-l1E?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  </span>
 </ModalVideo>
 
 <section id="App">
