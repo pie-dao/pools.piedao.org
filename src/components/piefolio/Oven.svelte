ed0d1dbbe04d007c4ba45e578318d1738797c233
Alessio Delmonti <alessio.d@gmail.com>
2021-04-02 11:34:56 +0100

    Making piefolio oven component better

--- src/components/piefolio/Oven.svelte
+++ src/components/piefolio/Oven.svelte
@@ -3,86 +3,14 @@
   import { ethers } from 'ethers';
   import filter from 'lodash/filter';
   import images from '../../config/images.json';
+  import ovensConf from '../../config/ovensConf.js';
   import { balances, balanceKey, eth } from '../../stores/eth.js';
   import { getTokenImage, subscribeToBalance, toFixed } from '../../components/helpers.js';
   import { fetchOvensUserData } from '../../helpers/multicall';
   import Modal from '../../components/elements/Modal.svelte';
   import OvenModal from '../../components/modals/OvenModal.svelte';
 
-  $: ovens = [
-    {
-      addressOven: '0x1d616dad84dd0b3ce83e5fe518e90617c7ae3915',
-      deprecated: false,
-      name: 'DEFI++ Oven',
-      description: 'Bakes DEFI++ at Zero cost',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'DEFI++',
-        address: '0x8d1ce361eb68e9e05573443c407d4a3bed23b033',
-        balance: '0',
-        icon: getTokenImage('0x8d1ce361eb68e9e05573443c407d4a3bed23b033'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    {
-      addressOven: '0xE3d74Df89163A8fA1cBa540FF6B339d13D322F61',
-      deprecated: false,
-      name: 'BCP Oven',
-      description: 'Bakes BCP at Zero cost',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'BCP',
-        address: '0xe4f726adc8e89c6a6017f01eada77865db22da14',
-        balance: '0',
-        icon: getTokenImage('0xe4f726adc8e89c6a6017f01eada77865db22da14'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    {
-      addressOven: '0xAedec86DeDe3DEd9562FB00AdA623c0e9bEEb951',
-      deprecated: false,
-      name: 'YPIE Oven',
-      description: 'Bakes YPIE at Zero cost',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'YPIE',
-        address: '0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31',
-        balance: '0',
-        icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    // {
-    //   addressOven: '0x925f860d1596cc6383c16294d8290f82bde172f7',
-    //   deprecated: true,
-    //   name: 'YPIE Oven',
-    //   description: 'Bakes YPIE at Zero cost',
-    //   data: {
-    //     ethBalance: 0,
-    //     pieBalance: 0,
-    //   },
-    //   baking: {
-    //     symbol: 'YPIE',
-    //     address: '0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31',
-    //     balance: '0',
-    //     icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31'),
-    //   },
-    //   highlight: true,
-    //   enabled: true,
-    // },
-  ];
+  $: ovens = ovensConf;
 
   let modal;
   let modalAdd;
@@ -144,15 +72,15 @@
           <div class="w-100pc rounded-xl bg-white p-6 mt-6">
             <div class="flex justify-between my-2 h-28px">
               <span class="font-thin text-left">Oven name</span>
-              <span class="font-bold text-right flex items-center"><img width="28px" height="28px" class="mr-2" src={images.bcp} alt="token name" />{oven.baking.symbol}</span>
+              <span class="font-bold text-right flex items-center"><img width="28px" height="28px" class="mr-2" src={oven.baking.icon} alt="token name" />{oven.baking.symbol}</span>
             </div>
             <div class="flex justify-between my-2 h-28px">
               <div class="flex items-center ">
                 <span class="font-thin text-left mr-2">Minimum reached</span>
               </div>
               <div class="font-bold text-right rounded-sm bg-black w-40pc h-24px">
-                <div class="px-2 h-24px flex items-center rounded-sm text-xs bg-gradient-purple text-left text-white fit-content">
-                  <span>{getPercetageCompletion($balances[oven.KeyEthBalance])}</span>
+                <div class="px-2 h-24px flex items-center rounded-sm text-xs bg-gradient-purple text-left text-white fit-content" style={`width: ${getPercetageCompletion($balances[oven.KeyEthBalance])}% !important`}>                  
+                  <span>{getPercetageCompletion($balances[oven.KeyEthBalance])}%</span>
                 </div>
               </div>
             </div>
--- src/pages/landings/oven.svelte
+++ src/pages/landings/oven.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import { ethers } from 'ethers';
   import images from '../../config/images.json';
+  import ovensConf from '../../config/ovensConf.js';
   import Meta from '../../components/elements/meta.svelte';
   import { balances, balanceKey, eth } from '../../stores/eth.js';
 
@@ -18,100 +19,7 @@
   import AccordionGroup from '../../components/elements/AccordionGroup.svelte';
 
 
-  $: ovens = [
-    {
-      addressOven: "0x0c4Ff8982C66cD29eA7eA96d985f36aE60b85B1C",
-      //addressOven: '0x90Cc6F4ec7Aa0468D2eDb3F627AcD988B14A78b4',
-      //addressOven: '0xb9Eef048dcc5F9CC453029cC2ed21f4a558ad0E8',
-      deprecated: false,
-      name: 'PLAY Oven',
-      description: 'Bakes PLAY with ETH',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'PLAY',
-        address: '0x33e18a092a93ff21ad04746c7da12e35d34dc7c4',
-        balance: '0',
-        icon: getTokenImage('0x33e18a092a93ff21ad04746c7da12e35d34dc7c4'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    {
-      addressOven: '0x1d616dad84dd0b3ce83e5fe518e90617c7ae3915',
-      deprecated: false,
-      name: 'DEFI++ Oven',
-      description: 'Bakes DEFI++ at Zero cost',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'DEFI++',
-        address: '0x8d1ce361eb68e9e05573443c407d4a3bed23b033',
-        balance: '0',
-        icon: getTokenImage('0x8d1ce361eb68e9e05573443c407d4a3bed23b033'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    {
-      addressOven: '0xE3d74Df89163A8fA1cBa540FF6B339d13D322F61',
-      deprecated: false,
-      name: 'BCP Oven',
-      description: 'Bakes BCP at Zero cost',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'BCP',
-        address: '0xe4f726adc8e89c6a6017f01eada77865db22da14',
-        balance: '0',
-        icon: getTokenImage('0xe4f726adc8e89c6a6017f01eada77865db22da14'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    {
-      addressOven: '0xAedec86DeDe3DEd9562FB00AdA623c0e9bEEb951',
-      deprecated: false,
-      name: 'YPIE Oven',
-      description: 'Bakes YPIE at Zero cost',
-      data: {
-        ethBalance: 0,
-        pieBalance: 0,
-      },
-      baking: {
-        symbol: 'YPIE',
-        address: '0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31',
-        balance: '0',
-        icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31'),
-      },
-      highlight: true,
-      enabled: true,
-    },
-    // {
-    //   addressOven: '0x925f860d1596cc6383c16294d8290f82bde172f7',
-    //   deprecated: true,
-    //   name: 'YPIE Oven',
-    //   description: 'Bakes YPIE at Zero cost',
-    //   data: {
-    //     ethBalance: 0,
-    //     pieBalance: 0,
-    //   },
-    //   baking: {
-    //     symbol: 'YPIE',
-    //     address: '0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31',
-    //     balance: '0',
-    //     icon: getTokenImage('0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31'),
-    //   },
-    //   highlight: true,
-    //   enabled: true,
-    // },
-  ];
+  $: ovens = ovensConf;
 
   let modal;
   let modalV2;
