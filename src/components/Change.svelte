b73e4dbc3e7eba910158f480fab846f2043d9fb6
Nicola Di Marco <ndimarco@amp.energy>
2021-04-16 17:49:14 +0100

    Change
    - got rid of the 24h label

--- src/components/Change.svelte
+++ src/components/Change.svelte
@@ -10,22 +10,23 @@
   n/a
 {:else}
   {#if value > 0}
-    <div class="flex justify-around items-center fitcontent text-green">
+    <div class="flex items-center justify-around fitcontent text-green">
       <img src={images.icons.ArrowUp} alt="close" class="h-12px mr-2px" />
       <div class="ml-2px font-thin">{value.toFixed(2)} %</div>
-      <div class="font-thin">{#if displayLabel} 24h{/if}</div>
+      <!-- <span class="font-thin">{#if displayLabel} 24h{/if}</span> -->
     </div>
   {:else if value == 0}
-  <div class="flex justify-around items-center fitcontent text-black">
+  <div class="flex  items-center justify-center fitcontent text-black">
   <!-- <div class="ml-2px font-thin">{value.toFixed(2)} %</div>
   <div class="font-thin">{#if displayLabel} 24h{/if}</div> -->
+  <div class="ml-2px font-thin">0</div>
   </div>
 
   {:else}
-  <div class="flex justify-around items-center fitcontent text-red">
+  <div class="flex items-center justify-center fitcontent text-red">
         <img src={images.icons.ArrowDown} alt="close" class="h-12px mr-2px" />
         <div class="ml-2px font-thin">{value.toFixed(2)} %</div>
-        <div class="font-thin">{#if displayLabel} 24h{/if}</div>
+        <!-- <span class="font-thin">{#if displayLabel} 24h{/if}</span> -->
     </div>
   {/if}
 {/if}
