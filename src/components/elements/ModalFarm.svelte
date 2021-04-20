1d1bf1fa0eab4f516241a8dc57a5601d3b433641
Nicola Di Marco <ndimarco@amp.energy>
2021-04-20 18:05:42 +0100

    click outside

--- src/components/elements/ModalFarm.svelte
+++ src/components/elements/ModalFarm.svelte
@@ -15,8 +15,8 @@
     </script>
     
     {#if modalIsOpen}
-        <div use:clickOutside on:click={close} class="genericmodal overflow-y-auto flex justify-center items-start md:items-center">
-            <div class="overflow-y-auto flex flex-col justify-start p-1px cardbordergradient w-100pc min-h-100pc  lg:max-w-60pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:mousedownOutside on:click_outside={() => modalIsOpen = false}>
+        <div class="genericmodal overflow-y-auto flex justify-center items-start md:items-center">
+            <div use:clickOutside on:click_outside={close} class="overflow-y-auto flex flex-col justify-start p-1px cardbordergradient w-100pc min-h-100pc  lg:max-w-60pc lg:min-w-30pc lg:max-h-80pc lg:h-80pc lg:min-h-50pc" use:mousedownOutside on:click_outside={() => modalIsOpen = false}>
     
                 <button on:click={close} class="w-30px h-30px ml-auto relative top-20px right-20px">
                     <img src={images.closebutton} alt="closebutton" />
