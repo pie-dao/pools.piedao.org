1438982377f484e04ed85cb214383d82439a2174
Totò Volturno <info@kunigu.tech>
2021-05-21 12:12:36 +0200

    fixed eslint error, made simulator eslit compatible

--- src/classes/farming_simulator/Calculator.js
+++ src/classes/farming_simulator/Calculator.js
@@ -1,11 +1,10 @@
 import { CoinGecko } from '../../stores/coingecko.js';
 
-export class Calculator {
-
+export default class Calculator {
   constructor() {
     // default treasury address, just a placeholder for now
     // TODO: change it for real address before go live...
-    this.treasury_address = "0x4efD8CEad66bb0fA64C8d53eBE65f31663199C6d";
+    this.treasury_address = '0x4efD8CEad66bb0fA64C8d53eBE65f31663199C6d';
 
     // default empty markets object, to be filled dynamically...
     this.markets = {
@@ -17,16 +16,16 @@ export class Calculator {
       dough: {
         price: 0,
         circSupply: 0,
-        marketCap: 0
-      }
+        marketCap: 0,
+      },
     };
 
     // default rewards object...
     this.rewards = {
       distributed: 60,
       compound: 25,
-      costs: 15
-    };    
+      costs: 15,
+    };
 
     // default constant...
     this.k = 56.0268900276223;
@@ -36,82 +35,81 @@ export class Calculator {
       treasury: {
         expectedYearlyReturns: 0,
         expectedMonthlyReturns: 0,
-        expectedApr: 0
+        expectedApr: 0,
       },
       user: {
         expectedYearlyReturns: 0,
         expectedMonthlyReturns: 0,
-        expectedApr: 0        
-      }
-    }     
+        expectedApr: 0,
+      },
+    };
   }
 
   initMarkets() {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       // if the markets object is already initialized, we simply return it back
       // avoiding making too many requests to coingecko considering that on input-change event
       // we shall recalculate the final user's profits...
-      if(this.markets.initialized) {
+      if (this.markets.initialized) {
         resolve(this.markets);
       } else {
-        try {
+        CoinGecko.fetchCoinData('piedao-dough-v2').then((doughResponse) => {
           // retrieving the infos about DOUGH from coingecko...
-          let dough_response = await CoinGecko.fetchCoinData("piedao-dough-v2");
-          let dough_market_data = dough_response.market_data;
+          const doughMarketData = doughResponse.market_data;
 
           // saving the values we need inside our config object...
-          this.markets.dough.price = dough_market_data.current_price.usd;
-          this.markets.dough.circSupply = dough_market_data.circulating_supply;
-          this.markets.dough.marketCap = dough_market_data.market_cap.usd;
-          
+          this.markets.dough.price = doughMarketData.current_price.usd;
+          this.markets.dough.circSupply = doughMarketData.circulating_supply;
+          this.markets.dough.marketCap = doughMarketData.market_cap.usd;
+
           // retrieving the balance of Treasury from address...
           // TODO: remove the hardcoded number and use API (Zapper, Zerion)...
           this.markets.treasuryLiquidity = 10000000;
 
           this.markets.initialized = true;
           resolve(this.markets);
-    
-          } catch(error) {
-            reject(error);
-          }        
+        }).catch((error) => reject(error));
       }
     });
   }
 
   calculate(inputs) {
-    return new Promise(async (resolve, reject) => {
-      this.project(inputs).then((projections) => {  
-        let user_yearly_returns = projections.returns.user.reduce((total, value) => total += value);
+    return new Promise((resolve, reject) => {
+      this.project(inputs).then((projections) => {
+        const userYearlyReturns = projections.returns.user.reduce((total, value) => total + value);
 
         this.outputs.user = {
-          expectedYearlyReturns: user_yearly_returns,
-          expectedAverageMontlyReturns: user_yearly_returns / 12,
-          expectedApr: (user_yearly_returns / (inputs.stakedDough * this.markets.dough.price)) * 100,
-          expectedVeDough: this.calculateVeDough(inputs.stakedDough, inputs.commitment)
+          expectedYearlyReturns: userYearlyReturns,
+          expectedAverageMontlyReturns: userYearlyReturns / 12,
+          expectedApr: (userYearlyReturns / (inputs.stakedDough * this.markets.dough.price)) * 100,
+          expectedVeDough: this.calculateVeDough(inputs.stakedDough, inputs.commitment),
         };
 
-        let treasury_yearly_returns = projections.farming.asset[12] - this.markets.treasuryLiquidity;        
+        const treasuryYearlyReturns = projections.farming.asset[12]
+          - this.markets.treasuryLiquidity;
 
         this.outputs.treasury = {
-          expectedYearlyReturns: treasury_yearly_returns,
-          expectedAverageMontlyReturns: treasury_yearly_returns / 12,
-          expectedApr: this.markets.treasuryLiquidity  / projections.farming.asset[12]
-        };        
+          expectedYearlyReturns: treasuryYearlyReturns,
+          expectedAverageMontlyReturns: treasuryYearlyReturns / 12,
+          expectedApr: this.markets.treasuryLiquidity / projections.farming.asset[12],
+        };
 
         // rounding the numbers of the returns object...
-        this.roundNumbers(this.outputs, null, 2);
+        Object.keys(this.outputs).forEach((key) => {
+          this.outputs[key] = this.roundNumbers(this.outputs[key], 2);
+        });
 
-        resolve({outputs: this.outputs, breakdowns: projections});
-      }).catch(error => reject(error));
+        resolve({ outputs: this.outputs, breakdowns: projections });
+      }).catch((error) => reject(error));
     });
   }
 
-  calculateVeDough(stakedDough, commitment) {    
+  calculateVeDough(stakedDough, commitment) {
     return stakedDough * this.calculateCommitmentMultiplier(commitment);
   }
 
   calculateCommitmentMultiplier(commitment) {
-    return commitment / this.k * Math.log10(commitment);
+    return commitment / (this.k * Math.log10(commitment));
   }
 
   calculateCompound(value) {
@@ -119,50 +117,59 @@ export class Calculator {
   }
 
   project(inputs) {
-    return new Promise(async(resolve, reject) => {
+    return new Promise((resolve, reject) => {
       this.initMarkets().then(() => {
-        let projections = {
+        const projections = {
           farming: {
             asset: [],
             gains: [],
             compounds: [],
-            staking_rewards: []
-          }, 
+            staking_rewards: [],
+          },
           returns: {
             per_ve_dough: [],
-            user: [] 
-          }
+            user: [],
+          },
         };
 
-        let farming_asset = this.markets.treasuryLiquidity;
-        let total_staked_ve_dough = inputs.stakedVeDough + this.calculateVeDough(inputs.stakedDough, inputs.commitment);
+        let farmingAsset = this.markets.treasuryLiquidity;
+
+        const totalStakedVeDough = inputs.stakedVeDough
+          + this.calculateVeDough(inputs.stakedDough, inputs.commitment);
 
         // calculating the projections for the next 12 months...
-        for(let i = 0; i < 13; i++) {
-          projections.farming.asset[i] = farming_asset;
-          projections.farming.gains[i] = (farming_asset * (inputs.expectedApr / 100)) / 12;
-          projections.farming.compounds[i] = projections.farming.gains[i] * (this.rewards.compound / 100);
-          projections.farming.staking_rewards[i] = i == 0 ? 0 : projections.farming.gains[i] * (this.rewards.distributed / 100);
-        
-          projections.returns.per_ve_dough[i] = projections.farming.staking_rewards[i] / (total_staked_ve_dough * (1 - (inputs.rewardsUnclaimed / 100)));
-          projections.returns.user[i] = projections.returns.per_ve_dough[i] * this.calculateVeDough(inputs.stakedDough, inputs.commitment);
-          
+        for (let i = 0; i < 13; i += 1) {
+          projections.farming.asset[i] = farmingAsset;
+          projections.farming.gains[i] = (farmingAsset * (inputs.expectedApr / 100)) / 12;
+
+          projections.farming.compounds[i] = projections.farming.gains[i]
+            * (this.rewards.compound / 100);
+
+          projections.farming.staking_rewards[i] = i === 0 ? 0
+            : projections.farming.gains[i] * (this.rewards.distributed / 100);
+
+          projections.returns.per_ve_dough[i] = projections.farming.staking_rewards[i]
+            / (totalStakedVeDough * (1 - (inputs.rewardsUnclaimed / 100)));
+
+          projections.returns.user[i] = projections.returns.per_ve_dough[i]
+            * this.calculateVeDough(inputs.stakedDough, inputs.commitment);
+
           // adding the montly compound into the farming asset...
-          farming_asset += this.calculateCompound(projections.farming.gains[i]);
+          farmingAsset += this.calculateCompound(projections.farming.gains[i]);
         }
 
         resolve(projections);
-      }).catch(error => reject(error));      
+      }).catch((error) => reject(error));
     });
   }
 
-  roundNumbers(object, key = "root", decimals = 4) {
-    Object.keys(object).forEach(key => {
-      if(typeof(object[key]) == "object") {
-        this.roundNumbers(object[key], key, decimals);
-      } else {
-        object[key] = Number.parseFloat(object[key]).toFixed(decimals);
-      }
+  static roundNumbers(object, decimals = 4) {
+    const roundedObject = {};
+
+    Object.keys(object).forEach((key) => {
+      roundedObject[key] = Number.parseFloat(object[key]).toFixed(decimals);
     });
+
+    return roundedObject;
   }
 }
--- src/classes/farming_simulator/Simulator.js
+++ src/classes/farming_simulator/Simulator.js
@@ -1,25 +1,24 @@
-import { Calculator } from './Calculator.js';
+import Calculator from './Calculator.js';
 
-export class Simulator {
-
+export default class Simulator {
   constructor() {
     // instance of the Calculator class...
     this.calculator = new Calculator();
   }
 
   calculate(inputs) {
-    return new Promise((resolve, reject) => {            
-      this.calculator.calculate(inputs).then(response => {
+    return new Promise((resolve, reject) => {
+      this.calculator.calculate(inputs).then((response) => {
         resolve(response);
-      }).catch(error => reject(error));
+      }).catch((error) => reject(error));
     });
   }
 
   retrieveMarkets() {
-    return new Promise((resolve, reject) => {            
-      this.calculator.initMarkets().then(markets => {
+    return new Promise((resolve, reject) => {
+      this.calculator.initMarkets().then((markets) => {
         resolve(markets);
-      }).catch(error => reject(error));
+      }).catch((error) => reject(error));
     });
   }
 }
