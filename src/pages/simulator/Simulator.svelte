df5b55de4707d67af9224444f0c89506e0ba47a9
Totò Volturno <info@kunigu.tech>
2021-05-31 14:32:45 +0200

    corrected typo

--- src/pages/simulator/Simulator.svelte
+++ src/pages/simulator/Simulator.svelte
@@ -96,7 +96,7 @@
   <div class="w-full px-8 max-w-1200px">
     <div class="font-hero font-hero-sim">New<br />Governance<br />Mining</div>
     <a href="#/placeholder" data-aos="fade-up" data-aos-delay="500"><button class="btnbig text-white m-0 my-8 rounded-8px p-15px min-w-200px w-100pc lg:w-200px lg:min-w-200px">Earn up to 45% APY</button></a>
-    <div class="text-base font-thin mx-4 md:mx-20pc mb-8">Invest in <strong>veDOUGH*</strong> and build your cashflow, taking additional yield from PieDAO famed treasury.</div>
+    <div class="text-base font-thin mx-4 md:mx-20pc mb-8">Invest in <strong>veDOUGH*</strong> and build your cashflow, taking additional yield from PieDAO farmed treasury.</div>
     <div class="font-huge">How it works</div>
     <LottiePlayer
     src="https://assets10.lottiefiles.com/private_files/lf30_wksf88hl.json"
