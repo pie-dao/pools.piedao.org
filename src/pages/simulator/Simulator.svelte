fd0f03bdbcd75c1f911f904aae4176671f8b3e8b
Nicola Di Marco <ndimarco@amp.energy>
2021-06-01 20:09:27 +0100

    buttons

--- src/pages/simulator/Simulator.svelte
+++ src/pages/simulator/Simulator.svelte
@@ -305,7 +305,7 @@
           </div>
           <div class="w-full flex flex-col">
             <div class="w-full font-thin text-left md:text-xs leading-2 mb-4">Your Staking Commitment</div>  
-            <div class="flex items-center bg-white rounded text-center w-full p-13px md:p-26px mb-8 md:mt-4">
+            <div class="flex items-center bg-white rounded text-center w-full p-10px mb-8 md:mt-4">
               <div class="w-1/4">
                 <button class:selected="{inputs.commitment === 6}" class="bg-button" on:click={() => changeCommitment(6)}>6 months</button>
               </div>
