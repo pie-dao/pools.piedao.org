1b1cf64b64d95ccedfab237136094e7685cd9ca4
Totò Volturno <info@kunigu.tech>
2021-06-02 17:41:06 +0200

    added modal component

---
+++ src/components/modals/infoModal.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { _ } from "svelte-i18n";
+    export let description_key;
+    </script>
+    
+  <div class="liquidity-container flex-col justify-items-center bg-grey-243 rounded-4px lg:px-4 lg:pb-4">
+    {$_(description_key)}
+  </div>../helpers.js../helpers.js
