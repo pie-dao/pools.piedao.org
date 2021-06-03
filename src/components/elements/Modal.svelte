3a954ec95647457497cc0f7ee14bcbfa3e71645a
Nicola Di Marco <ndimarco@amp.energy>
2021-06-03 12:48:24 +0100

    modal

--- src/components/elements/Modal.svelte
+++ src/components/elements/Modal.svelte
@@ -20,14 +20,21 @@ let modalIsOpen = false;
     <div class="genericmodal flex justify-center items-center">
         <div style={ backgroundColor ? `background-color: ${backgroundColor} !important` : "#fff"} 
              class="flex flex-col justify-start modalcontent w-100pc min-h-100pc p-2 overflow-x-hidden overflow-y-auto lg:max-w-600px lg:min-w-30pc lg:max-h-70pc lg:min-h-50pc" use:mousedownOutside on:click_outside={() => modalIsOpen = false}>
-            <div class="flex">
+         
              {#if title}
+             <div class="flex">
                 <h1 class="pl-50px text-center text-xl w-100pc"> {title} </h1>
+                <button on:click={close} class="w-30px h-30px mr-20px self-center">
+                    <img src={images.closebutton} alt="closebutton" />
+                </button>
+             </div>
+            {:else}
+            <div class="flex justify-end">
+                <button on:click={close} class="w-30px h-30px self-center">
+                    <img src={images.closebutton} alt="closebutton" />
+                </button>
+             </div>
             {/if}
-            <button on:click={close} class="w-30px h-30px mr-20px self-center">
-                <img src={images.closebutton} alt="closebutton" />
-            </button>
-        </div>
             <slot name="content">
                 
             </slot>
