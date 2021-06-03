2cb8be18fc56b7f3b3fb95a616b6a42175ebf139
Totò Volturno <info@kunigu.tech>
2021-06-03 12:43:08 +0200

    added slider modal

---
+++ src/components/modals/stakingCommitmentModal.svelte
@@ -0,0 +1,11 @@
+<script>
+    import RangeSlider from "svelte-range-slider-pips";
+    export let rewards;
+    </script>
+    
+  <div class="liquidity-container flex-col justify-items-center bg-grey-243 rounded-4px lg:px-4 lg:pb-4">
+    {#each rewards as reward}
+    <RangeSlider values={[reward.percentage]}/>
+    {reward.commitment}
+    {/each}
+  </div>
