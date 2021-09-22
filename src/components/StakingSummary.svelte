a8c98f16ea4a20e55ed76efeb3fe086ac5278e0e
Totò Volturno <info@kunigu.tech>
2021-09-22 16:14:27 +0200

    PIE-427 - fixed layout

--- src/components/StakingSummary.svelte
+++ src/components/StakingSummary.svelte
@@ -56,21 +56,24 @@
 <Modal title="You can't claim yet" backgroundColor="#f3f3f3" bind:this={modalinfo}>
   <div slot="content" class="font-thin text-center hidescrollbar">
     <p class="pb-2">Here's what you have to do:</p>
+
+    <div class="text-center mx-auto w-auto rounded-xl pointer mt-4 w-200px" style="border: 1px solid #FFAC32;">
+      <a href="https://snapshot.org/#/piedao" target="_blank">Snapshot/PieDAO ⚡</a>
+    </div>     
+    <p class="pt-2 mb-8">1. Vote on the current proposals<br />to be eligible to claim rewards</p>
+   
     <div class="text-center mx-auto">
       <img
       class="w-80px h-80px mx-auto"
       src={images.rewardsPie}
       alt="ETH"
     /> 
-    </div>
-    <p class="pt-2">1. Vote on the current proposals<br />to be eligible to claim rewards</p>
-    <div class="text-center mx-auto w-auto rounded-xl pointer mt-4 mb-4 w-200px" style="border: 1px solid #FFAC32;">
-      <a href="https://snapshot.org/#/piedao" target="_blank">Snapshot/PieDAO ⚡</a>
     </div>    
-    <p class="pt-2">2. Claim your rewards!<br />Rewards are calculated and available<br />for claiming at the start of every calendar month.</p>
+    <p class="pt-2 mb-4">2. Claim your rewards!<br />Rewards are calculated and available<br />for claiming at the start of every calendar month.</p>
+
     <div class="text-center mx-auto">
       <img
-      class="w-100px h-100px mx-auto p-4 token-icon "
+      class=" mx-auto pt-4 token-icon"
       src={images.simulator_sword}
       alt="ETH"
     /> 
