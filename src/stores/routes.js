78f0fa2996ea9daa5943a0912e76355b9b4fb04f
Alessio Delmonti <alessio.d@gmail.com>
2021-09-29 14:11:01 +0100

    Testing routing

--- src/stores/routes.js
+++ src/stores/routes.js
@@ -77,11 +77,36 @@ const formatRoute = (route) => {
   let poolAction;
   let referral;
   let method;
+
+  console.log('route', route, typeof route);
+  if (window.gtag) {
+    const trackPath = `/${route.join('/')}`;
+    window.gtag('event', 'page_view', {
+      page_path: trackPath,
+    });
+    console.log('Analytics DEV jdshaf', {
+      page_path: route[0],
+    });
+  }
+
+  const _route = route ? [...route] : [];
+  if (_route) {
+    for (let i = 0; i < _route.length; i++) {
+      if (_route[i] && _route[i].indexOf('?') >= 0) {
+        _route[i] = _route[i].substring(0, _route[i].indexOf('?'));
+      }
+    }
+    console.log('route[0]', _route[0])
+
+    
+  }
+
+  console.log('_route', _route);
   const notFound = { page: NotFound, params: { path: `/${route.join('/')}` } };
 
   // changeUrl(route);
 
-  switch (route[0] || 'root') {
+  switch (_route[0] || 'root') {
     case 'support':
       return { page: Redirect, params: { label: 'support page!', to: 'https://piedao.atlassian.net/servicedesk/customer/portals' } };
     case 'about':
@@ -93,10 +118,10 @@ const formatRoute = (route) => {
     case 'pies':
       return { page: Dashboard };
     case 'exp':
-      address = (route[1] || '0x992e9f1d29e2fdb57a9e09a78e122fafe3720cc5').toLowerCase();
+      address = (_route[1] || '0x992e9f1d29e2fdb57a9e09a78e122fafe3720cc5').toLowerCase();
       return { page: Experipie, params: { address } };
     case 'pie':
-      address = (route[1] || '').toLowerCase();
+      address = (_route[1] || '').toLowerCase();
       return { page: PiePageSwitch, params: { address } };
     case 'dough-staking-campaign':
       return { page: DoughStakingCampaign };
@@ -126,24 +151,24 @@ const formatRoute = (route) => {
     //   return { page: StakingRewards };
     case 'staking-simulator':
       /* eslint-disable no-case-declarations */
-      const simulation = (route[1] || '');
+      const simulation = (_route[1] || '');
       /* eslint-enable no-case-declarations */
       return { page: Simulator, params: { simulation } };
     case 'simulator-stats':
       return { page: SimulatorStats };
     case 'lp-legacy-farm':
-      referral = route[1] || null;
+      referral = _route[1] || null;
 
       if (referral) {
         window.localStorage.setItem('referral', referral);
       }
       return { page: LPStaking, params: { referral } };
     case 'staking':
-      return route[1] ? { page: StakingPageSingle, params: route } : { page: Staking };
+      return _route[1] ? { page: StakingPageSingle, params: _route } : { page: Staking };
     case 'pools':
-      address = (route[1] || '').toLowerCase();
-      poolAction = (route[2] || 'add').toLowerCase();
-      method = (route[3] || 'single').toLowerCase();
+      address = (_route[1] || '').toLowerCase();
+      poolAction = (_route[2] || 'add').toLowerCase();
+      method = (_route[3] || 'single').toLowerCase();
 
       if (pools.available.includes(address)) {
         return { page: Pool, params: { address, poolAction, method } };
@@ -179,16 +204,16 @@ window.addEventListener('hashchange', () => {
   const trackPath = `/${newRoute.join('/')}`;
 
   if (window.location.origin !== 'http://localhost:8080') {
-    window.gtag('event', 'page_view', {
-      page_path: trackPath,
-    });
+    // window.gtag('event', 'page_view', {
+    //   page_path: trackPath,
+    // });
   } else {
     console.log('Analytics DEV', {
       page_path: trackPath,
     });
   }
 
-  currentRoute.set({ ...formatRoute(newRoute) });
+  // currentRoute.set({ ...formatRoute(newRoute) });
   window.scrollTo({
     top: 0,
     left: 0,
