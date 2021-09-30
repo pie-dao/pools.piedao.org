aa3bdd31b38c724bbba894de0ef1c9df2959d414
Alessio Delmonti <alessio.d@gmail.com>
2021-09-30 13:08:04 +0200

    Redirect staking page to landing (#220)

--- src/stores/routes.js
+++ src/stores/routes.js
@@ -110,6 +110,7 @@ const formatRoute = (route) => {
     case 'pie':
       address = (_route[1] || '').toLowerCase();
       return { page: PiePageSwitch, params: { address } };
+    case 'dough-staking':
     case 'dough-staking-campaign':
       return { page: DoughStakingCampaign };
     case 'dough':
