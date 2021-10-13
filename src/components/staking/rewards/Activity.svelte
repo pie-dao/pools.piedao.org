6672f985b4fd96988bf318e09f76e7018c598a9c
Totò Volturno <info@kunigu.tech>
2021-10-13 18:02:19 +0200

    more work on governance component

--- src/components/staking/rewards/Activity.svelte
+++ src/components/staking/rewards/Activity.svelte
@@ -1,12 +1,65 @@
 <script>
+  import { eth } from '../../../stores/eth.js';
+  import { formatFiat } from '../../../components/helpers.js';
+  import { toNum} from '../../../helpers/staking.js';
+  import images from '../../../config/images.json';
+  import epochsJSON from '../../../config/epochs.json';
 
+  export let block;
+  let currentAddress;
+  let report;
+  let currentAccount;
+
+  $: if ($eth.address) {
+    console.log("currentAddress", currentAddress);
+    // if address is first setup, or is changed...
+    if (currentAddress !== $eth.address) {
+      currentAddress = $eth.address;
+
+      // TODO: this should be changed, fetch the epochs from backend
+      report = epochsJSON.epochs.find(epoch => epoch.startBlock == block);
+      currentAccount = report.merkleTree.leafs.find(leaf => leaf.staker.id == $eth.address);
+      console.log("currentAccount", currentAccount, $eth.address, report);      
+    }
+  }
 </script>
 
 <div class="flex flex-col items-center w-full md:w-1/2 p-1px bg-lightgrey rounded-16 m-10px">
   <div class="flex flex-col nowrap w-96pc m-2pc swap-from rounded-20px bg-white p-16px">
     <div class="font-huge text-center">Activity</div>
+    {#if $eth.address}
+      {#if currentAccount}
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <img class="h-auto w-24px mr-10px" src={images.veDough} alt="dough token" />
+            <span class="token-symbol-container">Your veDOUGH</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              {formatFiat(toNum(currentAccount.staker.accountVeTokenBalance), ',', '.', '')}
+            </div>        
+          </div>
+        </div>
+
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <span class="token-symbol-container">Voting Power</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              to be calculated
+            </div>        
+          </div>
+        </div>    
+      {:else}   
+        <div class="text-center p-1">
+          Sorry, there's no data for this account.
+        </div> 
+      {/if}
+    {:else}
     <div class="text-center p-1">
-      content goes here...
-    </div>
+      loading...
+    </div> 
+    {/if}
   </div>
 </div>
--- src/components/staking/rewards/Governance.svelte
+++ src/components/staking/rewards/Governance.svelte
@@ -1,24 +1,101 @@
 <script>
+  import { eth } from '../../../stores/eth.js';
   import { formatFiat } from '../../../components/helpers.js';
-  import { toNum, claim , getParticipations} from '../../../helpers/staking.js';
+  import { toNum, AVG_SECONDS_MONTH} from '../../../helpers/staking.js';
   import images from '../../../config/images.json';
+  import epochsJSON from '../../../config/epochs.json';
 
-  export let accountVeTokenBalance;
+  export let block;
+  let currentAddress;
+  let report;
+  let currentAccount;
+  let totalVeDoughVoted;
+
+  $: if ($eth.address) {
+    // if address is first setup, or is changed...
+    if (currentAddress !== $eth.address) {
+      currentAddress = $eth.address;
+
+      // TODO: this should be changed, fetch the epochs from backend
+      report = epochsJSON.epochs.find(epoch => epoch.startBlock == block);
+      currentAccount = report.merkleTree.leafs.find(leaf => leaf.staker.id == $eth.address);
+      
+      totalVeDoughVoted = report.participants.reduce((previousValue, currentValue) => {
+        return {score: previousValue.score + currentValue.score};
+      });
+    }
+  }
 </script>
 
 <div class="flex flex-col items-center w-full md:w-1/2 p-1px bg-lightgrey rounded-16 m-10px">
   <div class="flex flex-col nowrap w-96pc m-2pc swap-from rounded-20px bg-white p-16px">
     <div class="font-huge text-center">Governance</div>
-    <div class="flex flex-row p-1 justify-between items-center">
-      <div class="flex items-center">
-        <img class="h-auto w-24px mr-10px" src={images.veDough} alt="dough token" />
-        <span class="token-symbol-container">Your veDOUGH</span>          
-      </div>
-      <div class="flex flex-col items-right">
-        <div class="font-24px">
-          {formatFiat(toNum(accountVeTokenBalance), ',', '.', '')}
-        </div>        
-      </div>
-    </div>
+    {#if $eth.address}
+      {#if currentAccount}
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <img class="h-auto w-24px mr-10px" src={images.veDough} alt="dough token" />
+            <span class="token-symbol-container">Total veDOUGH</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              {formatFiat(toNum(report.stakingStats.veTokenTotalSupply), ',', '.', '')}
+            </div>        
+          </div>
+        </div>
+
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <span class="token-symbol-container">Avg Lock Time</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              {Math.floor(report.stakingStats.averageTimeLock / AVG_SECONDS_MONTH)}
+            </div>        
+          </div>
+        </div>  
+        
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <span class="token-symbol-container">N of proposals this months</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              {report.proposals.length}
+            </div>        
+          </div>
+        </div>  
+        
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <span class="token-symbol-container">Participation Ration</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              to be calculated
+            </div>        
+          </div>
+        </div>  
+        
+        <div class="flex flex-row p-1 justify-between items-center">
+          <div class="flex items-center">
+            <span class="token-symbol-container">Total veDOUGH Voted</span>          
+          </div>
+          <div class="flex flex-col items-right">
+            <div class="font-24px">
+              {formatFiat(totalVeDoughVoted.score, ',', '.', '')}
+            </div>        
+          </div>
+        </div>         
+      {:else}   
+        <div class="text-center p-1">
+          Sorry, there's no data for this account.
+        </div> 
+      {/if}
+    {:else}
+    <div class="text-center p-1">
+      loading...
+    </div> 
+    {/if}
   </div>
 </div>
