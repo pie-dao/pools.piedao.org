812ac0188e8b292229b3d2a0b41367e4fc3a69c2
Nicola Di Marco <ndimarco@amp.energy>
2021-10-13 14:09:59 +0200

    aligning content

--- src/components/StakingRewardGovernance.svelte
+++ src/components/StakingRewardGovernance.svelte
@@ -9,9 +9,9 @@
 <div class="flex flex-col items-center w-full md:w-1/2 p-1px bg-lightgrey rounded-16 m-10px">
   <div class="flex flex-col nowrap w-96pc m-2pc swap-from rounded-20px bg-white p-16px">
     <div class="font-huge text-center">Governance</div>
-    <div class="flex flex-row text-center p-1">
-      <div class="flex flex-col items-left">
-        <img class="h-auto w-24px mx-5px" src={images.veDough} alt="dough token" />
+    <div class="flex flex-row p-1 justify-between items-center">
+      <div class="flex items-center">
+        <img class="h-auto w-24px mr-10px" src={images.veDough} alt="dough token" />
         <span class="token-symbol-container">Your veDOUGH</span>          
       </div>
       <div class="flex flex-col items-right">
