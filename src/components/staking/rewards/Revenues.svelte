67f1764e5808c036a6c89267715d03a2822900a6
Totò Volturno <info@kunigu.tech>
2021-10-15 15:29:18 +0200

    fixed decimals

--- src/components/staking/rewards/Revenues.svelte
+++ src/components/staking/rewards/Revenues.svelte
@@ -44,7 +44,7 @@
           </div>
           <div class="flex flex-col items-right">
             <div class="">
-              {(underlying.fiat ? underlying.fiat: "") + " " + underlying.amount} 
+              {(underlying.fiat ? underlying.fiat: "") + " " + underlying.amount.toFixed(2)} 
             </div>        
           </div>
         </div>
