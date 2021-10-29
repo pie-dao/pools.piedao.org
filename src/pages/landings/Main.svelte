35bc3e8a9e49af5f871801847af61a2d33b44238
Totò Volturno <info@kunigu.tech>
2021-10-29 12:14:08 +0200

    added back kpi-options banner on home page

--- src/pages/landings/Main.svelte
+++ src/pages/landings/Main.svelte
@@ -376,6 +376,20 @@
   <div class="block md:hidden">
     <img class="w-100% inline mb-4" src={images.doughconomics} alt="dough economics diagram" />
   </div>
+
+  <a class="hidden md:block mt-8" href="#/kpi-options-campaign">
+    <LottiePlayer
+    src="https://assets7.lottiefiles.com/private_files/lf30_wvaae9to.json"
+    autoplay="{true}"
+    loop="{true}"
+    controls="{false}"
+    renderer="svg"
+    background="white"
+    height=""
+    width="100%"
+    controlsLayout="{controlsLayout}"
+    />
+  </a>
 </div>
 
 <Newsletter />
