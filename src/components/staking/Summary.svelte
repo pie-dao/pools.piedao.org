1ca96eae95aa81b94b2df98328eb542e34a78323
Totò Volturno <info@kunigu.tech>
2021-11-01 09:21:21 +0100

    hidden kpi-options box when rewards are zero

--- src/components/staking/Summary.svelte
+++ src/components/staking/Summary.svelte
@@ -149,24 +149,24 @@
     {/if}
     </div>
   </div>
-  <div
-  class="flex flex-col nowrap w-92pc mx-4pc mt-4 swap-from rounded-20px bg-white p-16px"
->
-  <div class="flex items-center justify-between">
-    <div class="flex nowrap intems-center p-1 font-thin">Estimated UMA KPI Options</div>
-  </div>
-  <div class="flex nowrap items-center p-1">
-    <div class="flex-1">
-      <span class="sc-iybRtq gjVeBU">
-        <div class="font-24px">
-          {eth.address ? formatFiat(data.estimatedKpiOptions, ',', '.', '') : 0}
-        </div>
-        <img class="h-auto w-24px mx-5px" src={images.doughtoken} alt="dough token" />
-        <span class="sc-kXeGPI jeVIZw token-symbol-container">DOUGH</span>
-      </span>        
+  {#if !data.estimatedKpiOptions.eq(0)}
+  <div class="flex flex-col nowrap w-92pc mx-4pc mt-4 swap-from rounded-20px bg-white p-16px">
+    <div class="flex items-center justify-between">
+      <div class="flex nowrap intems-center p-1 font-thin">Estimated UMA KPI Options</div>
+    </div>
+    <div class="flex nowrap items-center p-1">
+      <div class="flex-1">
+        <span class="sc-iybRtq gjVeBU">
+          <div class="font-24px">
+            {eth.address ? formatFiat(data.estimatedKpiOptions, ',', '.', '') : 0}
+          </div>
+          <img class="h-auto w-24px mx-5px" src={images.doughtoken} alt="dough token" />
+          <span class="sc-kXeGPI jeVIZw token-symbol-container">DOUGH</span>
+        </span>        
+      </div>
     </div>
   </div>
-</div>
+  {/if}
   <div class="w-full flex px-4 md:px-6">
     <div class="w-1/2 flex flex-col flex-shrink mr-2 mt-4 mb-6 rounded-20px bg-white p-16px"
     >
