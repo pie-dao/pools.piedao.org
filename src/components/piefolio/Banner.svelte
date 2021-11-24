16f23c29a4aad19b26edc39708a1c3f2e1e29f77
Alessio Delmonti <alessio.d@gmail.com>
2021-11-24 12:02:15 +0000

    Adding piefolio to menu + fixing links

--- src/components/piefolio/Banner.svelte
+++ src/components/piefolio/Banner.svelte
@@ -2,7 +2,7 @@
   import images from "../../config/images.json";
 </script>
 
-  <a class="" href="#/dough">
+  <a class="" href="#/swap">
     <div class="rounded-xl">
       <img width="100%" height="auto" class="rounded-xl" src="https://raw.githubusercontent.com/pie-dao/brand/master/misc/buydough-banner.png" alt="external link icon" />
     </div>
