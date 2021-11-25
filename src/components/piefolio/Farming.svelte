d774c01513958a8bf12d9d39f2c0da8eba9a103d
Totò Volturno <info@kunigu.tech>
2021-11-25 09:24:36 +0100

    fixed amount bug on claim modal

--- src/components/piefolio/Farming.svelte
+++ src/components/piefolio/Farming.svelte
@@ -54,9 +54,9 @@
 
   async function fetchRewardEscrowData() {
     try {
-      // const address = '0x5ce583b0431794f65ee97fbd6fffacf2adad344c';
+      const address = '0x5ce583b0431794f65ee97fbd6fffacf2adad344c';
       // const address = '0xc96265c36F6D77747f9c259946a1eF55FcE946b7';
-      const address = $eth.address;
+      // const address = $eth.address;
 
       if (!address) {
         return;
@@ -132,7 +132,7 @@
   }
 
   async function claim() {
-    claimModal.showModal(doughInEscrow);
+    claimModal.showModal(claimableDoughInEscrow);
   }
 </script>
 
--- src/notifications.js
+++ src/notifications.js
@@ -16,23 +16,48 @@ if(env.customRPC.enabled) {
   // for eavery block update we receive from the custom RPC...
   provider.on("block", async(blockNumber) => {
     try {
-    // we retrieve the block infos...
-    let block = await provider.getBlockWithTransactions(blockNumber);
-    // we get the emitters froms store writables...
-    let emittersArray = get(emitters);
-    // for each transaction in the current block...
-    block.transactions.forEach(async(transaction) => {
-      // we search for a match in the emitersArray...
-      let tnx = emittersArray.find(el => el.hash == transaction.hash);
-      // if a transaction was stored into our emitersArray...
-      if(tnx) {
-        // we fetch the transaction receipt, and check the status...
-        let tnxReceipt = await provider.getTransactionReceipt(transaction.hash);
-        tnx.emitter.emit(tnxReceipt);
-      }
-    });
+      // we retrieve the block infos...
+      let block = await provider.getBlockWithTransactions(blockNumber);
+      // we get the emitters froms store writables...
+      let emittersArray = get(emitters);
 
-    console.log("block event", block.transactions, blockNumber, emittersArray);      
+      console.log("block event", block.transactions, blockNumber, emittersArray);
+
+      // for each transaction in the current block...
+      block.transactions.forEach(async(transaction) => {
+        // we search for a match in the emitersArray...
+        let tnx = emittersArray.find(el => el.hash.toLowerCase() === transaction.hash.toLowerCase());
+        // if a transaction was stored into our emitersArray...
+        if(tnx) {
+          // we fetch the transaction receipt, and check the status...
+          let tnxReceipt = await provider.getTransactionReceipt(transaction.hash);
+
+          if(tnxReceipt.status == 1) {
+            await tnx.emitter.emit({...tnxReceipt, eventCode: 'txConfirmed'});
+          } else {
+            await tnx.emitter.emit({...tnxReceipt, eventCode: 'txFailed'});
+
+            displayNotification({
+              autoDismiss: 15000,
+              message: 'Sorry, something went wrong.',
+              type: 'error',
+            });            
+          }
+
+          // removing pending notification from DOM...
+          let pendingNotifications = document.getElementsByClassName("bn-notify-notification-pending");
+          for (let item of pendingNotifications) {
+              item.remove();  
+          }
+
+          // cleaning up emittersArray after firing the event...
+          const index = emittersArray.indexOf(tnx);
+          if (index > -1) {
+            emittersArray.splice(index, 1);
+          }
+          emitters.set(emittersArray);
+        }
+      });    
     } catch(error) {
       console.log("block event: error", error);
     }
