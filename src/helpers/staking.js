a148338b3b82303b05c64f981b6a1be53dee5f95
Totò Volturno <info@kunigu.tech>
2021-12-22 14:22:35 +0100

    added 3 deciamls to votingPower

--- src/helpers/staking.js
+++ src/helpers/staking.js
@@ -386,7 +386,6 @@ export const fetchStakingData = async (eth) => {
   // this is a fallback in case the graph is not working...
   try {
     // using graph...
-    console.log("using address", eth.address);
     response = await fetchStakingDataGraph(eth.address);
 
     if (response.stakers.length) {
