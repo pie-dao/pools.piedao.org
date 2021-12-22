8727b2cb7d6f745b0e27fc046fd5662dfcb68398
Totò Volturno <info@kunigu.tech>
2021-12-22 14:55:52 +0100

    fixed date timezones

--- src/pages/StakingRewardBreakdown.svelte
+++ src/pages/StakingRewardBreakdown.svelte
@@ -9,8 +9,6 @@
   // TODO: this should be changed, fetch the epochs from backend
   let epoch = epochsJSON.epochs.find(epoch => epoch.merkleTree.windowIndex == $currentRoute.params.windowIndex);
   let date = moment(new Date(epoch.startDate * 1000));
-  console.log("epoch startDate", epoch.startDate);
-  console.log("epoch date", date.format());
 </script>
 
 <div class="flex w-100pc py-20px flex flex-col items-center">
@@ -20,7 +18,7 @@
 
   <div class="flex flex-col items-center w-full md:w-1/2 p-1px bg-lightgrey rounded-16 m-10px">
     <div class="flex flex-col nowrap w-96pc m-2pc swap-from rounded-20px bg-white p-16px">
-      <div class="font-huge text-center pb-4">{date.format()}</div>
+      <div class="font-huge text-center pb-4">{moment(date).utc().add(date.utcOffset(), 'm').format('MMMM YYYY')}</div>
       <div class="text-center p-1 font-thin">
         Epoch Blocks Start: {epoch.startBlock} - Epoch Blocks End: {epoch.endBlock}
       </div>
