4e7f9bc5c53269b12400e1e8e10f5986679a5254
AdrianAddesso <87126850+AdrianAddesso@users.noreply.github.com>
2022-05-23 11:01:49 -0300

    Defi++ copy update post Intervention
    Updating the content on the Pie page for DeFi++ as reviewed at https://www.notion.so/piedao/DeFi-content-update-03455fa3597c46e6911ada127558fbe9

--- src/components/marketing-elements/dxp-explanation.svelte
+++ src/components/marketing-elements/dxp-explanation.svelte
@@ -14,15 +14,15 @@
 <div>
     <h1 class="font-lg">Summary:</h1>
     <div class="font-thin">
-    Over the last year Decentralized Finance has exploded, with many new financial tools built on Ethereum.
-    DEFI Index Pie is the most diversified DeFi index on the market, aiming to provide the broadest possible exposure to this rapidly growing industry.  
+    Over the last few years Decentralized Finance has exploded, with many new financial tools built on Ethereum.
+    DEFI Index Pie is the most diversified DeFi index on the market, aiming to provide the broadest possible exposure to this rapidly growing industry, with a focus on market leaders and innovators.  
     <br/><br/>
     To achieve this we included both large and small market cap projects, with a focus on market leaders and innovators. The pie is a 70/30 weighted balance of our DLP and DSP indices. 
     </div>
 
     <h1 class="mt-4">Objective:</h1>
     <div class="font-thin">
-        As a community we pooled our knowledge to build an index containing the highest-quality projects at the top of their game, featuring the best large and small cap projects in the ecosystem. A wide range of financial services are represented, including decentralized stablecoins (Maker), derivatives (Synthetix), price oracles (ChainLink) and layer two scaling solutions (LoopRing).
+        As a community we pooled our knowledge to build an index containing the highest-quality projects at the top of their game, featuring the best projects in the ecosystem. A wide range of financial services are represented, including decentralized stablecoins (Maker), derivatives (Synthetix), price oracles (ChainLink) and layer two scaling solutions (LoopRing).
         <br/><br/>
         DeFi users can easily gain access to a diverse spectrum of DeFi projects with DEFI++, able to zap in at the touch of a button and just one transaction.
         <br/><br/>
@@ -38,21 +38,21 @@
         Why diversify? Our backtesting shows that the more diversified an index, the better it performs. Not only that wide diversification ensures minimal risk exposure, meaning DEFI++ is the safest DeFi index too. 
         <br/><br/>
 
-        Following the same process as all our indices, DEFI++’s allocation is the result of weeks of community research and discussion, ultimately being voted into existence as a 70/30 balanced allocation of our Large & Small Caps indices.
+        Following the same process as all our indices, DEFI++’s allocation is the result of community research and discussion held within the community, before being voted into existence.
     </div>
 
     
     <h1 class="mt-4">Operation:</h1>
 
     <div class="font-thin">
-        DEFI++ is a tokenized representation of a basket of relevant assets, with the underlying tokens safely stored in our treasury. The index is constructed using Balancer smart pools meaning that it constantly rebalances and reacts to market changes in real-time. There is no withdrawal fee, and a highly competitive 0.7% streaming fee rewards DOUGH holders for their governance of the system.
+        DEFI++ is a tokenized representation of a basket of relevant assets, with the underlying tokens safely stored in our treasury. The index is constructed using the PieVault technology. There is no withdrawal fee, and a highly competitive 0.7% streaming fee rewards DOUGH holders for their governance of the system.
         <br/><br/>
         We also provide a DEFI++ staking contract where users can provide liquidity in return for a steady stream of DOUGH.
     </div>
 
     <h1 class="mt-4">Performance:</h1>
     <div class="font-thin">
-        Above you can see how the DEFI++ index performed relative to its underlying assets and Ether. Harry Markowitz was right, diversification really is the only free lunch in finance.
+        Overall, DEFI++ index performed relative to its underlying assets and Ether seems to confirm Harry Markowitz’s theory: diversification is the only free lunch in finance.
     </div>
     
 </div>
