87f531d879ee555e1ce30d402d4b7dbb2631980a
KnxPwr <gabrielebonumore@gmail.com>
2022-06-02 16:02:49 +0200

    feat(DoughStaking): add how to governance video on staking page

---
+++ src/components/HowToGovernance.svelte
@@ -0,0 +1,7 @@
+<script>
+    import images from '../config/images.json';
+</script>
+
+<div class="flex w-full cursor-pointer">
+    <img src={images.how_to_governance} alt="How pieDAO Governance video explanation" class="w-full" />
+</div>
---
+++ src/components/HowToGovernanceVideo.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { elasticOut } from 'svelte/easing'
+
+    function Transition() {
+        return {
+            duration: 2000,
+            css(tick) {
+                tick = elasticOut(tick)
+                return `
+                    opacity: ${tick};
+                `
+            },
+        }
+    }
+</script>
+
+<div class="videoWrapper w-full" in:Transition>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/_ef6IkZUCu0?fs=1&autoplay=1&mute=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
