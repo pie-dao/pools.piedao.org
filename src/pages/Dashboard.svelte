31d965e247118d39b1566115e77552c7166b92ab
KnxPwr <gabrielebonumore@gmail.com>
2022-06-03 17:14:29 +0200

    Tweak config to show PLAY on tab but not on swap

--- src/pages/Dashboard.svelte
+++ src/pages/Dashboard.svelte
@@ -142,20 +142,12 @@
               </td>
               <td class="border px-4 ml-8 py-2 font-thin text-center">
                 {#if pie.buyButton}
-                  {#if pie.useMintOverBuy}
-                      <a href={`#/pie/${pie.address}`}>
-                        <button class="table-btn highlight-box min-w-70px">
-                          {pie.symbol}
-                        </button>
-                      </a>
-                  {:else}
                     <a href={pie.buyUrl ? pie.buyUrl : `#/swap`} target={pie.buyUrl ? "_blank" : "_self"}>
                       <button class="table-btn highlight-box min-w-70px">
                         {pie.symbol}
                       </button>
                     </a>
                   {/if}
-                {/if}
               </td>
               
             </tr>
