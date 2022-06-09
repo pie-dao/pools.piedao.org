338e3e85a8da49a64a540bf2e950d1a09ee4c4ef
KnxPwr <gabrielebonumore@gmail.com>
2022-06-09 20:21:26 +0200

    fix(Summary): restore notarizing check & enable claim during testing from fork rpc

--- src/components/staking/Summary.svelte
+++ src/components/staking/Summary.svelte
@@ -17,7 +17,8 @@
     import BigNumber from "bignumber.js";
     import sliceDoughData from "../../config/slice-dough.json";
     import { getMerkleTreeDistributorContract } from "../kpi-options/kpiUtils";
-    import { BigNumber as BigNumberEthers, constants } from "ethers"
+    import { BigNumber as BigNumberEthers, constants } from "ethers";
+    import env from "../../config/env.json";
     
     let claimModal;
     let compoundModal;
@@ -216,7 +217,7 @@
         const nextCreatedIndex = await merkleTreeDistributorContract.nextCreatedIndex();
         const currentWindowIndex = nextCreatedIndex.sub(BigNumberEthers.from(1))
         const currentWindow = await merkleTreeDistributorContract.merkleWindows(currentWindowIndex);
-        isNotarizing = false;
+        isNotarizing = currentWindow.merkleRoot !== constants.AddressZero;
     }
 
     onMount(async () => {
@@ -384,7 +385,7 @@
             on:click={() => compoundModal.open()}
         >Compound</button>
             <button 
-                disabled={isLoading || isNotarizing}
+                disabled={isLoading || (isNotarizing && !env.customRPC.enabled)}
                 class="flex items-center bg-black rounded-xl pointer px-4 py-2 text-white claim-button"
                 on:click={() => {
                     if($eth.address) {
