060c62c3546da1799344cf5cd1eb41ae8db45115
KnxPwr <gabrielebonumore@gmail.com>
2022-07-01 14:48:59 +0200

    improve calculations on total balance

--- src/helpers/zapper.js
+++ src/helpers/zapper.js
@@ -1,5 +1,5 @@
 import { fetchEventSource } from '@microsoft/fetch-event-source';
-
+import { isEmpty } from 'lodash';
 const zapperApiUrl = 'https://api.zapper.fi/v2';
 const treasury = '0x3bcf3db69897125aa61496fc8a8b55a5e3f245d5';
 const zapperApiKey = '07809e9c-6d2d-4f6b-ba09-f3f7fb3e1fed';
@@ -9,7 +9,8 @@ export async function fetchTreasuryBalance() {
   /* eslint-enable import/prefer-default-export */
 
   try {
-    let treasuryBalance = 0;
+    let totalBalance = 0;
+    const balanceByProtocol = new Map();
     await fetchEventSource(
       `${zapperApiUrl}/balances?addresses%5B%5D=${treasury}&api_key=${zapperApiKey}`,
       {
@@ -17,16 +18,22 @@ export async function fetchTreasuryBalance() {
           if (event === 'balance') {
             const { app, appId, totals } = JSON.parse(data);
             if (app && app.meta && app.meta.total) {
-              treasuryBalance += parseFloat(app.meta.total);
-            } else if (appId === 'tokens' && totals) {
+              balanceByProtocol.set(appId, app.meta.total);
+            } else if (appId === 'tokens' && !isEmpty(totals)) {
               const tokenTotal = totals.reduce((n, { balanceUSD }) => n + balanceUSD, 0);
-              treasuryBalance += tokenTotal;
+              const networkKey = totals[0].network;
+              balanceByProtocol.set(networkKey, tokenTotal);
             }
+          }
+          if (event === 'end') {
+            balanceByProtocol.forEach((balance) => {
+              totalBalance += parseFloat(balance);
+            });
           }
         },
       },
     );
-    return treasuryBalance;
+    return totalBalance;
   } catch (e) {
     console.error('ERROR on zapper', e);
     return Promise.resolve(17000000);
