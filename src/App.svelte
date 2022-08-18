3998254ba3b8f2be8e0ff593ee198d3f6d10b2a5
Alessio Delmonti <alessio.d@gmail.com>
2022-08-18 16:52:03 +0100

    Removing firebase (#359)

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import "./styles.css";
   import AOS from 'aos';
   import 'aos/dist/aos.css'; 
-  import Analytics from "./components/elements/Analytics.svelte";
   import Footer from "./components/Footer.svelte";
   import Header from "./components/Header.svelte";
   import { currentRoute } from "./stores/routes.js";
@@ -21,7 +20,6 @@
 
 <section id="App">
   <Header />
-  <Analytics id={'G-DFT6D7P7L3'} />
   <svelte:component this={$currentRoute.page} params={$currentRoute.params} />
   <Footer />
 </section>
